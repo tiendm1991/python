1c1c15cc0fa1baab3eea503435583ceed5b274b7
TienDM <tiendm1991@gmail.com>
2019-05-12 17:15:36 +0700

    update

---
+++ history.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Tue Apr  2 22:10:10 2019)---
+module(3)
+modulus(3)
+runfile('C:/Users/TienDM/.spyder-py3/temp.py', wdir='C:/Users/TienDM/.spyder-py3')
+modulus(3)
+modulus(3.03)
+runfile('C:/Users/TienDM/.spyder-py3/temp.py', wdir='C:/Users/TienDM/.spyder-py3')
+x = 3
+x = 2
+x
+x = "123456789"
+x[3]
+x[3:]
+a = [1,2,3,4,5]
+a
+b = a[:]
+b
+*b
+first, *b, last = b
+b
+a
+a[1:-2]
+a[1:-1]
+
+## ---(Mon Apr 15 21:52:01 2019)---
+w = "axczxcz"
+w2 = "xitetr"
+[x for x in w]
+[x for x in w if x not in w2]
+str([x for x in w if x not in w2])
+"".join([x for x in w if x not in w2])
+{x for x in w if x not in w2}
+w1 = "program"
+w2 = "develop"
+{x for x in w1 if x not in w2}
+list({x for x in w1 if x not in w2})
+"".join({x for x in w1 if x not in w2})
+", ".join({x for x in w1 if x not in w2})
+list({x for x in w1 if x not in w2})
+sorted({x for x in w1 if x not in w2})
+"".join(sorted({x for x in w1 if x not in w2}))
+
+## ---(Sun Apr 21 09:37:04 2019)---
+range(0, 5, 0.5)
+runfile('C:/Users/TienDM/.spyder-py3/temp.py', wdir='C:/Users/TienDM/.spyder-py3')
+s = '12345'
+str(a[1])
+str(s[1])
+str(s[1:])
+a = '123'
+a.length
+a.length()
+len(data)
+len(a)
+runfile('C:/Users/TienDM/.spyder-py3/temp.py', wdir='C:/Users/TienDM/.spyder-py3')
+a = '123'
+a[0,2]
+runfile('C:/Users/TienDM/.spyder-py3/temp.py', wdir='C:/Users/TienDM/.spyder-py3')
+
+## ---(Tue Apr 23 21:46:12 2019)---
+s = '123'
+s.split()
+s.split('')
+s.split(' ')
+s
+[i for i in s]
+
+## ---(Tue Apr 23 21:58:10 2019)---
+s = 'a'
+s += 'b'
+s
+x = 'a'
+x = 'b'
+x - 'a'
+int(x)
+ord(x)
+x = ord('h') - ord('a')
+x
+
+## ---(Sun May  5 15:25:44 2019)---
+runfile('C:/Users/TienDM/.spyder-py3/temp.py', wdir='C:/Users/TienDM/.spyder-py3')
+
+## ---(Sun May 12 16:33:42 2019)---
+runfile('C:/Users/TienDM/.spyder-py3/temp.py', wdir='C:/Users/TienDM/.spyder-py3')
+a = [1,2,3,4,5]
+sorted(a)
+runfile('C:/Users/TienDM/.spyder-py3/temp.py', wdir='C:/Users/TienDM/.spyder-py3')
