c1303fc46511cba8ecfc9600e1554ac49a5ea1fc
tiendm <tiendm1991@gmail.com>
2019-09-09 10:33:44 +0700

    minJump

---
+++ DynamicPrograming/minJumps.py
@@ -0,0 +1,11 @@
+def minJumps(arr):
+    n = len(arr)
+    dp = [n] * n
+    dp[0] = 1
+    for i in range(1, n):
+        for j in range(i):
+            if(j + arr[j] >= i):
+                dp[i] = min(dp[i], dp[j]+1)
+    return dp[n-1]-1
+
+print(minJumps([1, 3, 6, 1, 0, 9]))
