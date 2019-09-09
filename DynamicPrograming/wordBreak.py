8c6ebff83f13083cbda060d4e5b464f09ddfe107
tiendm <tiendm1991@gmail.com>
2019-09-09 12:24:30 +0700

    workBreakDP

---
+++ DynamicPrograming/wordBreak.py
@@ -0,0 +1,17 @@
+d = {'i', 'like', 'sam', 'sung', 'samsung', 'mobile', 'ice', 'cream', 'icecream', 'mango', 'and'}
+def wordBreak(s):
+    n = len(s)
+    dp = [False for i in range(n+1)]
+    dp[0] = True
+    for i in range(1, n+1):
+        for j in range(i):
+            x = s[j:i]
+            if x not in d:
+                dp[i] = False
+            else:
+                dp[i] = dp[j]
+                if dp[j]:
+                    break
+    print(dp)
+    return dp[n]
+print(wordBreak('samsungandmango'))
