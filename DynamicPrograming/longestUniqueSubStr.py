ab5a3172fdbc80c99083814762218cc28f5b1942
tiendm <tiendm1991@gmail.com>
2019-10-10 16:45:44 +0700

    update

---
+++ DynamicPrograming/longestUniqueSubStr.py
@@ -0,0 +1,29 @@
+def longestUniqueSubStr(s):
+    n = len(s)
+    d = {s[0]: 0}
+    maxGlobal = 1
+    maxLocal = 1
+    start = 0
+    d = {s[0]: 0}
+    for i in range(1,n):
+        c = s[i]
+        if c not in d:
+            maxLocal += 1
+        else:
+            if d[c] < start:
+                maxLocal += 1
+            else:
+                maxGlobal = max(maxGlobal, maxLocal)
+                start = d[c] + 1
+                maxLocal = i - start + 1
+        d[c] = i
+        maxGlobal = max(maxGlobal, maxLocal)
+    return maxGlobal
+
+print(longestUniqueSubStr('GEEKSFORGEXS'))
+print(longestUniqueSubStr('GEEKSFORGEEKS'))
+print(longestUniqueSubStr('ABDEFGABEF'))
+print(longestUniqueSubStr('BBBB'))
+print(longestUniqueSubStr('BBBBAX'))
+
+
