cbfbe06f39f067622c9cb97c63cc2f42350d24b9
tiendm <tiendm1991@gmail.com>
2019-10-17 13:41:34 +0700

    update

---
+++ Backtracking/DivisionPossible2.py
@@ -0,0 +1,27 @@
+def DivisionPossible(arr, k):
+    s, ma, n = sum(arr), max(arr), len(arr)
+    if s % k != 0:
+        return False
+    avg = s // k
+    if ma > avg:
+        return False
+    subset = [0] * k
+    return help(arr, subset, avg, 0)
+
+def help(arr, subset, avg, idx):
+    n = len(arr)
+    k = len(subset)
+    if idx == n:
+        return True
+    x = arr[idx]
+    for i in range(k):
+        if subset[i] + x <= avg:
+            subset[i] += x
+            if help(arr, subset, avg, idx+1):
+                return True
+            subset[i] -= x
+    return False
+print(DivisionPossible([1,1,1,2,3,4,5,3], 4))
+print(DivisionPossible([2, 4, 1, 3, 2], 3))
+print(DivisionPossible([2,2,2,3], 3))
+
