d23afff3b9b3850eec93b7b33b71b9de84c10511
tiendm <tiendm1991@gmail.com>
2019-10-25 17:03:37 +0700

    update

---
+++ DynamicPrograming/longestRepeatedSubstring.py
@@ -0,0 +1,21 @@
+def longestRepeatedSubstring(s):
+    n = len(s)
+    dp = [[0 for j in range(n+1)] for j in range(n+1)]
+    for i in range(1, n):
+        c1 = s[i-1]
+        for j in range(i+1, n+1):
+            c2 = s[j-1]
+            if c2 == c1 and j - dp[i-1][j-1] > i:
+                dp[i][j] = dp[i-1][j-1] + 1
+    idx, m = 0, 0
+    for i in range(1,n+1):
+        x = max(dp[i])
+        if x > m:
+            m = x
+            idx = i
+    return s[idx - m:idx]
+
+print(longestRepeatedSubstring('aabaaba'))
+# aabaaba
+#012345678
+
