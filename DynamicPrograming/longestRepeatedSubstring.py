4eede1b1aa203d4716d3c6fc5f009de2006d3ada
tiendm <tiendm1991@gmail.com>
2019-10-25 17:09:57 +0700

    update

--- DynamicPrograming/longestRepeatedSubstring.py
+++ DynamicPrograming/longestRepeatedSubstring.py
@@ -5,8 +5,11 @@ def longestRepeatedSubstring(s):
         c1 = s[i-1]
         for j in range(i+1, n+1):
             c2 = s[j-1]
-            if c2 == c1 and j - dp[i-1][j-1] > i:
-                dp[i][j] = dp[i-1][j-1] + 1
+            if c2 == c1:
+                if j - dp[i-1][j-1] > i:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = dp[i - 1][j - 1]
     idx, m = 0, 0
     for i in range(1,n+1):
         x = max(dp[i])
