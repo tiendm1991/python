40e61656fa4ae87a369345214b6b25323f9d3545
TienDM <tiendm1991@gmail.com>
2020-02-22 10:35:54 +0700

    update

--- leetcode/construct-target-array-with-multiple-sums.py
+++ leetcode/construct-target-array-with-multiple-sums.py
@@ -1,40 +1,21 @@
 class Solution:
-    def isPossibleBacktrack(self, target) -> bool:
-        n = len(target)
-        if n == 1:
-            return target[0] == 1
-        def backtrack(target, arr, s):
-            check = True
-            for i in range(len(arr)):
-                if arr[i] > target[i]:
-                    return False
-                elif arr[i] < target[i]:
-                    check = False
-            if check:
-                return True
-            for i in range(len(arr)):
-                tmp = arr[i]
-                arr[i] = s
-                if backtrack(target, arr, 2 * s - tmp):
-                    return True
-                arr[i] = tmp
-            return False
-        return backtrack(target, [1] * len(target), len(target))
+    def shortestPalindrome(self, s: str) -> str:
+        if len(s) <= 1:
+            return s
 
-    def isPossibleFaster(self, target) -> bool:
-        n = len(target)
-        if n == 1:
-            return target[0] == 1
-        def help(target):
-            target.sort()
-            s = sum(target[:n-1])
-            if target[-1] == 1:
-                return True
-            elif target[-1] <= s:
-                return False
-            else:
-                target[-1] -= s
-                return help(target)
-        return help(target)
+        def isPalidrome(s, end):
+            return s[:end+1] == s[end::-1]
+
+        def findMaxPalindrome(s):
+            for i in range(len(s) - 1, 0, -1):
+                if isPalidrome(s, i):
+                    return i
+            return 0
+
+        idx = findMaxPalindrome(s)
+        add = ''
+        for i in range(len(s) - 1, idx, -1):
+            add += s[i]
+        return add + s
 s = Solution()
-print(s.isPossibleFaster([1, 13, 19]))
+print(s.shortestPalindrome('aacecaaa'))
