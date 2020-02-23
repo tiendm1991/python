8111d036271255e6f90fa297788e12b793c05ec6
TienDM <tiendm1991@gmail.com>
2020-02-23 11:32:18 +0700

    update

--- leetcode/contest/Contest2.py
+++ leetcode/contest/Contest2.py
@@ -1,5 +1,17 @@
+import math
 class Solution:
-    def isAdditiveNumber(self, num: str) -> bool:
-        return True
+    def closestDivisors(self, num: int):
+        a, b = num + 1, num + 2
+        x = int(math.ceil(math.sqrt(b)))
+        while x > 0:
+            if a % x == 0:
+                y = a // x
+                return [min(x,y), max(x, y)]
+            if b % x == 0:
+                y = b // x
+                return [min(x,y), max(x, y)]
+            x -= 1
+        return None
+
 s = Solution()
-print(s.isAdditiveNumber('0235813'))
+print(s.closestDivisors(999))
--- leetcode/contest/Contest3.py
+++ leetcode/contest/Contest3.py
@@ -1,5 +1,35 @@
 class Solution:
-    def isAdditiveNumber(self, num: str) -> bool:
-        return True
+    def validateBinaryTreeNodes(self, n: int, leftChild, rightChild) -> bool:
+        if leftChild[0] == -1 and rightChild[0] == -1:
+            return False
+        for x in leftChild:
+            if x != -1 and x in rightChild:
+                return False
+        parent = {}
+        for i in range(len(leftChild)):
+            if leftChild[i] == -1:
+                continue
+            if i in parent and parent[i] == leftChild[i]:
+                return False
+            parent[leftChild[i]] = i
+        parent = {}
+        for i in range(len(rightChild)):
+            if rightChild[i] == -1:
+                continue
+            if rightChild[i] in parent and parent[rightChild[i]] == i:
+                return False
+            parent[rightChild[i]] = i
+        tree = set()
+        stack = [0]
+        while len(stack) > 0:
+            par = stack.pop()
+            tree.add(par)
+            if rightChild[par] != -1:
+                stack.append(rightChild[par])
+            if leftChild[par] != -1:
+                stack.append(leftChild[par])
+        return len(tree) == n
+
+
 s = Solution()
-print(s.isAdditiveNumber('0235813'))
+print(s.validateBinaryTreeNodes(6, [1,-1,-1,4,-1,-1], [2,-1,-1,5,-1,-1]))
