8f89383f5ace6304be401422ca45455793acbda5
tiendm <tiendm1991@gmail.com>
2020-02-24 17:10:35 +0700

    update

---
+++ leetcode/coinChange.py
@@ -0,0 +1,26 @@
+import functools
+from datetime import datetime, time
+import math
+
+class Solution:
+    def coinChange(self, coins, amount: int) -> int:
+        if amount == 0:
+            return 0
+        coins.sort()
+        n = len(coins)
+        dp = [[-1 for j in range(amount+1)] for i in range(n)]
+        for i in range(n):
+            coin = coins[i]
+            for a in range(1, amount+1):
+                dp[i][a] = dp[i-1][a]
+                if a % coin == 0:
+                    dp[i][a] = a // coin
+                elif a > coin and dp[i][a-coin] != -1:
+                    dp[i][a] = min(dp[i][a], dp[i][a-coin] + 1) if dp[i][a] != -1 else dp[i][a-coin]+1
+        return dp[n-1][amount]
+
+s = Solution()
+startTime = datetime.now()
+print(s.coinChange([2,4,7], 15))
+print(datetime.now() - startTime)
+
---
+++ leetcode/verify-preorder-serialization-of-a-binary-tree.py
@@ -0,0 +1,29 @@
+import functools
+from datetime import datetime, time
+import math
+
+
+class Solution:
+    def isValidSerialization(self, preorder: str) -> bool:
+        order = preorder.split(',')
+        n = len(order)
+        def valid(s):
+            if s[0] == '#':
+                return len(s) == 1
+            newS = [s[0]]
+            for i in range(1, len(s)):
+                if s[i] != '#' or newS[i-1] != '#':
+                    newS.append(s[i])
+                else:
+                    newS.pop()
+                    newS[-1] = '#'
+                    if i < len(s):
+                        newS += s[i+1:]
+                    return valid(newS)
+            return False
+        return valid(order)
+s = Solution()
+startTime = datetime.now()
+print(s.isValidSerialization("1, #"))
+print(datetime.now() - startTime)
+
