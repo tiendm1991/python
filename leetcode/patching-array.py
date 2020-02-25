352116eb25fc43aeff17d813cb5dac2366d2c2f2
TienDM <tiendm1991@gmail.com>
2020-02-25 20:33:17 +0700

    update

---
+++ leetcode/patching-array.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minPatches(self, nums, n: int) -> int:
+        _maxCover = 1
+        step = 0
+        count = 0
+        idx = 0
+        _maxCover = 0
+        while _maxCover < n:
+            if idx == len(nums):
+                count += 1
+                _maxCover = _maxCover * 2 + 1
+            elif _maxCover < nums[idx] - 1:
+                count += 1
+                _maxCover = _maxCover * 2 + 1
+            else:
+                _maxCover += nums[idx]
+                idx += 1
+            step += 1
+        return count
+
+
+s = Solution()
+print(s.minPatches([1, 2, 31, 33], 2147483647))
