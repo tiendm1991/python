605861c57484d8c015b75eefbb4cbddd4143916b
TienDM <tiendm1991@gmail.com>
2020-02-26 18:35:11 +0700

    update

--- leetcode/canMeasureWater.py
+++ leetcode/canMeasureWater.py
@@ -10,22 +10,15 @@ class Solution:
         if x == 0 or y == 0:
             return x + y == z
         if x == y:
-            return x == z or y == z or x+y == z
+            return x == z or y == z or x + y == z
         if x + y <= z:
             return x + y == z
         if x > y:
             x, y = y, x
-        def recusive(a):
-            if a == z % x:
-                return True
-            n = y - x + a
-            if a == n % x:
-                return False
-            return recusive(n % x)
-        return recusive(y % x)
+        return z % math.gcd(x, y) == 0
 
 s = Solution()
 startTime = datetime.now()
-print(s.canMeasureWater(6,9,1))
+print(s.canMeasureWater(4, 6, 8))
 print(datetime.now() - startTime)
 
