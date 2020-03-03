9b31fcda8f61a76adaf39a1485e29165f1494a44
tiendm <tiendm1991@gmail.com>
2020-03-03 16:07:23 +0700

    update

---
+++ leetcode/getMoneyAmount.py
@@ -0,0 +1,28 @@
+import functools
+from datetime import datetime, time
+import math
+import random
+
+class Solution:
+    def getMoneyAmount(self, n: int) -> int:
+        MAX_INTEGER = float('inf')
+        dp = [[MAX_INTEGER for j in range(n+1)]for i in range(n+1)]
+        for i in range(n, 0, -1):
+            for j in range(i, n+1):
+                if i == j:
+                    dp[i][j] = 0
+                elif j - i == 1:
+                    dp[i][j] = i
+                elif j - i == 2:
+                    dp[i][j] = (i + j) // 2
+                else:
+                    for k in range((i + j) // 2, j):
+                        dp[i][j] = min(dp[i][j], k + max(dp[k+1][j], dp[i][k-1]))
+        return dp[1][n]
+
+
+s = Solution()
+startTime = datetime.now()
+print(s.getMoneyAmount(17))
+print(datetime.now() - startTime)
+
---
+++ leetcode/lastRemaining.py
@@ -0,0 +1,28 @@
+import functools
+from datetime import datetime, time
+import math
+import random
+
+class Solution:
+    def lastRemaining(self, n: int) -> int:
+        leftToRight = True
+        start, end, u = 1, n, 1
+        while start < end:
+            n = (end - start) // u + 1
+            if n % 2 == 1:
+                start += u
+                end -= u
+            elif leftToRight:
+                start += u
+            else:
+                end -= u
+            u *= 2
+            leftToRight = not leftToRight
+        return start
+
+
+s = Solution()
+startTime = datetime.now()
+print(s.lastRemaining(100))
+print(datetime.now() - startTime)
+
