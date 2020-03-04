7da4259e2673ed4dab62db1a47377003674fbae5
tiendm <tiendm1991@gmail.com>
2020-03-04 16:09:23 +0700

    update

---
+++ leetcode/removeKdigits.py
@@ -0,0 +1,26 @@
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        n = len(num)
+        if k == n:
+            return '0'
+        stack = [num[0]]
+        for i in range(1, n):
+            while stack[-1] > num[i] and k > 0:
+                stack.pop()
+                k -= 1
+            stack.append(num[i])
+        while k > 0:
+            stack.pop()
+            k -= 1
+        i = 0
+        while i < len(stack) and stack[i] == '0':
+            i += 1
+        if i == len(stack):
+            return '0'
+        return ''.join(stack[i:])
+
+s = Solution()
+startTime = datetime.now()
+print(s.removeKdigits('1234567890', 4))
+print(datetime.now() - startTime)
+
