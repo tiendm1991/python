0b556065c99d3fe80bd7a10d9c0fbbd0c788a3c2
TienDM <tiendm1991@gmail.com>
2020-03-16 18:40:46 +0700

    update

--- leetcode/Solution.py
+++ leetcode/Solution.py
@@ -1,40 +1,19 @@
 from  datetime import datetime
 class Solution:
-    a = 1
-    def makesquare(self, nums) -> bool:
-        n = len(nums)
-        s = sum(nums)
-        if s == 0 or s % 4 != 0:
-            return False
-        s //= 4
-        m = max(nums)
-        if m > s:
-            return False
-        nums = sorted(nums, reverse=True)
-        def backtrack(a, visited, idx):
-            self.a += 1
-            print('step ' + str(self.a))
-            if idx == 3:
-                return True
-            for i in range(n):
-                if visited[i]:
-                    continue
-                if a[idx] + nums[i] > s:
-                    break
-                elif a[idx] + nums[i] <= s:
-                    a[idx] += nums[i]
-                    visited[i] = True
-                    tmp = idx + 1 if a[idx] == s else idx
-                    if backtrack(a, visited, tmp):
-                        return True
-                    a[idx] -= nums[i]
-                    visited[i] = False
-            return False
+    def nextGreaterElement(self, nums1, nums2):
+        stack, d = [], {}
+        for num in nums2:
+            if not stack or stack[-1] > num:
+                stack.append(num)
+                continue
+            while len(stack) > 0 and stack[-1] < num:
+                d[stack.pop()] = num
+            stack.append(num)
+        for num in stack:
+            d[num] = -1
+        return [d[num] for num in nums1]
 
-        arr = [0] * 4
-        visited = [False] * n
-        return backtrack(arr, visited, 0)
 s = Solution()
 startTime = datetime.now()
-print(s.makesquare([5,5,5,5,16,4,4,4,4,4,3,3,3,3,4]))
+print(s.nextGreaterElement([4,1,2], [1,3,4,2]))
 print(datetime.now() - startTime)
