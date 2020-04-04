1a1fb73be62c2f86f49a15a9edf312659cceb11d
TienDM <tiendm1991@gmail.com>
2020-04-04 17:08:01 +0700

    update

--- leetcode/numDecodings.py
+++ leetcode/numDecodings.py
@@ -15,4 +15,19 @@ class Solution:
                     dp[i] += dp[i+2]
                 else:
                     dp[i] += 1
+
+        def numDecodings2(self, s: str) -> int:
+            n = len(s)
+            if n == 0:
+                return 0
+            dp = [0 for i in range(n + 1)]
+            dp[-1] = 1
+            for i in range(n - 1, -1, -1):
+                if s[i] == '0':
+                    dp[i] = 0
+                    continue
+                dp[i] = dp[i + 1]
+                if i < n - 1 and int(s[i] + s[i + 1]) <= 26:
+                    dp[i] += dp[i + 2]
+            return dp[0]
         return dp[0]
