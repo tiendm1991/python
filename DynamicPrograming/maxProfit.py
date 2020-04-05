d7d345e4ae2acd554bb3cd6c6693afd083bbb106
TienDM <tiendm1991@gmail.com>
2020-04-05 17:46:17 +0700

    update

--- DynamicPrograming/maxProfit.py
+++ DynamicPrograming/maxProfit.py
@@ -1,21 +1,15 @@
-def maxProfit(arr, k):
-    n = len(arr)
-    dp = [[0 for i in range(n)] for j in range(k+1)]
-    for i in range(1, k+1):
-        miIdx = (i-1)*2
-        mi = arr[miIdx]
-        for j in range(i * 2 - 1, n):
-            x1 = dp[i-1][j]
-            x2 = dp[i][j-1]
-            x3 = max(arr[j] - mi, 0) + dp[i-1][miIdx-1]
-            dp[i][j] = max(x1, x2, x3)
-            #dp[i][j] = max(dp[i-1][j], dp[i][j-1], max(arr[j] - mi, 0) + dp[i-1][miIdx-1])
-            if arr[j] < mi:
-                mi = arr[j]
-                miIdx = j
-    return dp
-print(maxProfit([12, 14, 17, 10, 14, 13, 12, 15], 3))
-# 12-17 + 10-14 + 12-15 = 12
-print(maxProfit([10, 22, 5, 75, 65, 80], 2))
-print(maxProfit([100, 30, 15, 10, 8, 25, 80], 3))
+class Solution:
+    def maxProfit(self, prices) -> int:
+        if not prices:
+            return 0
+        s = 0
+        smallestIdx = 0
+        prices.append(prices[-1] - 1)
+        for i in range(1, len(prices)):
+            if prices[i] < prices[i-1]:
+                s += prices[i-1] - prices[smallestIdx]
+                smallestIdx = i
+        return s
 
+s = Solution()
+print(s.maxProfit([1,6,5,4,2,3,4,5]))
