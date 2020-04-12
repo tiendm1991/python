3f5d7305bc03798f11b76521ce071241a0fbe659
TienDM <tiendm1991@gmail.com>
2020-04-12 10:52:42 +0700

    update

---
+++ leetcode/queries-on-a-permutation-with-key.py
@@ -0,0 +1,16 @@
+class Solution:
+    def processQueries(self, queries, m: int):
+        d = {i + 1: i for i in range(m)}
+        result = []
+        for q in queries:
+            idx = d[q]
+            result.append(idx)
+            for k in d:
+                if k == q:
+                    d[k] = 0
+                elif d[k] < idx:
+                    d[k] += 1
+        return result
+
+s = Solution()
+print(s.processQueries([3,1,2,1], 5))
