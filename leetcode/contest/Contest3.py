c2c0445cc7dacd9feabe21575446a5651aaa7a07
TienDM <tiendm1991@gmail.com>
2020-04-12 10:43:35 +0700

    update

--- leetcode/contest/Contest3.py
+++ leetcode/contest/Contest3.py
@@ -1,34 +1,24 @@
 import collections
+
+
 class Solution:
-    def longestDiverseString(self, a: int, b: int, c: int) -> str:
-        def build(s, ca, cb, cc):
-            x, y, z = a-ca, b -cb, c - cc
-            _max = max(x,y,z)
-            if _max == x:
-                if ca < a and s[-2:] != 'aa':
-                    return build(s + 'a', ca+1, cb, cc)
-                elif y >= z:
-                    if cb < b and s[-2:] != 'bb':
-                        return build(s + 'b', ca, cb + 1, cc)
-                elif cc < c and s[-2:] != 'cc':
-                    return build(s + 'c', ca, cb, cc+1)
-            if _max == y:
-                if cb < b and s[-2:] != 'bb':
-                    return build(s + 'b', ca, cb + 1, cc)
-                elif x >= z:
-                    if ca < a and s[-2:] != 'aa':
-                        return build(s + 'a', ca + 1, cb, cc)
-                elif cc < c and s[-2:] != 'cc':
-                    return build(s + 'c', ca, cb, cc+1)
-            if _max == z:
-                if cc < c and s[-2:] != 'cc':
-                    return build(s + 'c', ca, cb, cc+1)
-                elif x >= y:
-                    if ca < a and s[-2:] != 'aa':
-                        return build(s + 'a', ca + 1, cb, cc)
-                elif cb < b and s[-2:] != 'bb':
-                    return build(s + 'b', ca, cb + 1, cc)
-            return s
-        return build('', 0, 0, 0)
+    def numOfWays(self, n: int) -> int:
+        mod = 10**9 + 7
+        if n == 1:
+            return 12
+        if n == 2:
+            return 54
+        preResult = 54
+        two = 30
+        three = 24
+        step = 2
+        while step < n:
+            two = (two * 3 + three * 2) % mod # two = twoPre * 3 + threePre * 2
+            three = (preResult * 2) % mod # three = twoPre * 2 + threePre * 2 = preResult * 2
+            preResult = (two + three) % mod # update current result of step, result at step = preResult
+            step += 1
+        return preResult
+
+
 s = Solution()
-print(s.longestDiverseString(5,4,3))
+print(s.numOfWays(5000))
