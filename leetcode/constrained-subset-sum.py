22ac167718a6c62df0070762ac154234ce69fd83
TienDM <tiendm1991@gmail.com>
2020-04-26 19:59:48 +0700

    update

--- leetcode/constrained-subset-sum.py
+++ leetcode/constrained-subset-sum.py
@@ -9,40 +9,28 @@ class Solution:
             for j in range(max(i - k, 0), i):
                 dp[i] = max(dp[i], dp[j] + nums[i])
         return max(dp)
-    def constrainedSubsetSum_Clear(self, nums, k: int) -> int:
+    def constrainedSubsetSum(self, nums, k: int) -> int:
         n = len(nums)
         if n == 1:
             return nums[0]
         dp = nums[::]
         deque = collections.deque()
         if dp[0] >= 0:
-            deque.append(dp[0])
+            deque.append(0)
         for i in range(1, n):
             dp[i] = nums[i]
             if deque:
-                dp[i] += deque[0]
-            if len(deque) == k:
+                dp[i] += dp[deque[0]]
+            if deque and deque[-1] - deque[0] == k-1:
                 deque.popleft()
-            while deque and (deque[-1] < dp[i] or deque[-1] < 0):
+            while deque and (dp[deque[-1]] < dp[i] or dp[deque[-1]] < 0):
                 deque.pop()
             if dp[i] >= 0:
-                deque.append(dp[i])
+                deque.append(i)
         return max(dp)
 
-    def constrainedSubsetSum_Short(self, nums, k: int) -> int:
-        n = len(nums)
-        if n == 1:
-            return nums[0]
-        dp = nums[::]
-        deque = collections.deque()
-        deque.append(max(dp[0], 0))
-        for i in range(1, n):
-            dp[i] = nums[i] + deque[0]
-            if len(deque) == k:
-                deque.popleft()
-            while deque and deque[-1] <= dp[i]:
-                deque.pop()
-            deque.append(max(dp[i], 0))
-        return max(dp)
 s = Solution()
+print(s.constrainedSubsetSum([10,2,-10,5,20], 2))
+print(s.constrainedSubsetSum([-1,-2,-3], 1))
 print(s.constrainedSubsetSum([10,-2,-10,-5,20], 2))
+print(s.constrainedSubsetSum([7, -5, -4, -3, 9], 3))
