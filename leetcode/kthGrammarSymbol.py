e0da20f1d1ad20ae9d0003cebf44bf658616aeb2
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-05-12 09:22:37 +0700

    recursion

---
+++ leetcode/kthGrammarSymbol.py
@@ -0,0 +1,13 @@
+class Solution:
+    def kthGrammar(self, N: int, K: int) -> int:
+        l = 2 ** (N-1)
+        def biSearch(n, k):
+            if k == 1:
+                return 0
+            elif k == 2:
+                return 1
+            if k <= n // 2:
+                return biSearch(n//2, k)
+            else:
+                return 1 - biSearch(n // 2, k - n//2)
+        return biSearch(l, K)
