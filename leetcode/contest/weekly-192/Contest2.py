6d7ff7bdf4acc9fd2c61d1c6fc1b01941275cdf6
TienDM <tiendm1991@gmail.com>
2020-06-07 17:51:52 +0700

    update

--- leetcode/contest/weekly-192/Contest2.py
+++ leetcode/contest/weekly-192/Contest2.py
@@ -1,15 +1,20 @@
 import functools
+
+
 class Solution:
     def getStrongest(self, arr, k: int):
         arr.sort()
         n = len(arr)
-        m = arr[(n-1) // 2]
+        m = arr[(n - 1) // 2]
+
         def cmp(x1, x2):
-            if abs(x1-m) != abs(x2 - m):
-                return abs(x1-m) - abs(x2-m)
+            if abs(x1 - m) != abs(x2 - m):
+                return abs(x1 - m) - abs(x2 - m)
             return x1 - x2
+
         a = sorted(arr, key=functools.cmp_to_key(cmp), reverse=True)
         return a[:k]
 
+
 s = Solution()
-print(s.getStrongest([1,2,3,4,5], 2))
+print(s.getStrongest([1, 2, 3, 4, 5], 2))
--- leetcode/contest/weekly-192/Contest3.py
+++ leetcode/contest/weekly-192/Contest3.py
@@ -1,25 +1,29 @@
 class BrowserHistory:
     def __init__(self, homepage: str):
-        self.history = {0:homepage}
+        self.history = {0: homepage}
         self._max = 0
         self._current = 0
+
     def visit(self, url: str) -> None:
         self._current += 1
         if self._current not in self.history or self.history[self._current] != url:
             self._max = self._current
             self.history[self._current] = url
+
     def back(self, steps: int) -> str:
         if self._current - steps < 0:
             self._current = 0
             return self.history[0]
         self._current -= steps
         return self.history[self._current]
+
     def forward(self, steps: int) -> str:
         if self._current + steps > self._max:
             self._current = self._max
             return self.history[self._max]
         self._current += steps
         return self.history[self._current]
+
 
 browserHistory = BrowserHistory("momn.com")
 browserHistory.visit("bx.com")
