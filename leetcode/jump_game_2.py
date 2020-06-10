e843e0d4ee6bd249ba9a765fad80c80a401974e1
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-06-10 12:13:10 +0700

    Create jump_game_2.py

---
+++ leetcode/jump_game_2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def jump(self, nums) -> int:
+        n = len(nums)
+        jump = [n] * n
+        q = [0]
+        jump[0] = 0
+        start = 1
+        while(q):
+            newQ = []
+            for i in q:
+                for j in range(start, min(i + nums[i] + 1, n)):
+                    if j == n-1:
+                        return jump[i] + 1
+                    if jump[j] == n:
+                        jump[j] = jump[i] + 1
+                        newQ.append(j)
+            start = min(i + nums[i] + 1, n)
+            q = newQ
+        return jump[n-1]
+s = Solution()
+print(s.jump([2,3,1,1,4]))
