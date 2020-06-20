794bb0a3efd7171359ff80f6ce8679bb486f9d0e
TienDM <tiendm1991@gmail.com>
2020-06-20 17:25:31 +0700

    update

--- leetcode/getKthPermutation.py
+++ leetcode/getKthPermutation.py
@@ -10,15 +10,17 @@ class Solution:
             return s
 
         result = ''
+        _set = {i for i in range(1, n+1)}
         while len(result) < n:
             a = calPermutation(n - len(result) - 1)
             x = k // a if k % a == 0 else k // a + 1
-            count, c = 0, 0
-            while count < x:
-                c += 1
-                if str(c) not in result:
-                    count += 1
-            result += str(c)
+            count = 1
+            for c in _set:
+                if count == x:
+                    result += str(c)
+                    _set.remove(c)
+                    break
+                count += 1
             k = k % a if k % a != 0 else a
         return result
 
