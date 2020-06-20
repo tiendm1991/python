252d509881a46868458ea944e2e4ca5cec43516d
TienDM <tiendm1991@gmail.com>
2020-06-20 17:30:01 +0700

    update

--- leetcode/getKthPermutation.py
+++ leetcode/getKthPermutation.py
@@ -4,10 +4,10 @@ from datetime import datetime
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
         def calPermutation(n):
-            s = 1
-            for i in range(2, n + 1):
-                s *= i
-            return s
+            pers = 1
+            for i in range(2, n+1):
+                pers *= i
+            return pers
 
         result = ''
         _set = {i for i in range(1, n+1)}
@@ -21,6 +21,22 @@ class Solution:
                     _set.remove(c)
                     break
                 count += 1
+            k = k % a if k % a != 0 else a
+        return result
+
+    def getPermutation_short(self, n: int, k: int) -> str:
+        def calPermutation(p):
+            pers = 1
+            for i in range(2, p+1):
+                pers *= i
+            return pers
+        result = ''
+        nums = [i for i in range(1,n+1)]
+        while len(result) < n:
+            a = calPermutation(n - len(result) - 1)
+            x = k // a if k % a == 0 else k // a + 1
+            result += str(nums[x-1])
+            del nums[x-1]
             k = k % a if k % a != 0 else a
         return result
 
