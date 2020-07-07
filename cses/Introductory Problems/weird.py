258e27e8adf8c9ee6118eb2a3641b27091879ee3
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-07-07 09:08:26 +0700

    Create weird.py

---
+++ cses/Introductory Problems/weird.py
@@ -0,0 +1,13 @@
+def f(n):
+    result = ""
+    while n > 1:
+        result += str(n) + " "
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = n*3+1
+    return result + "1"
+
+
+n = int(input())
+print(f(n))
