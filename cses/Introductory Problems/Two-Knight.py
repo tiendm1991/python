ba51b1731922c698b3ebbcc2b6ab6a066e2a746b
TienDM <tiendm1991@gmail.com>
2020-07-10 21:53:07 +0700

    update

--- cses/Introductory Problems/Two-Knight.py
+++ cses/Introductory Problems/Two-Knight.py
@@ -9,3 +9,12 @@ for i in range(5, n+1):
 for i in range(1, n+1):
 	x = i ** 2
 	print(x * (x-1) // 2 - dp[i])
+# Explain
+# total: total way to choose 2 cell from board (n * n) * (n * n - 1) // 2 (2Cn)
+# check the board n-4 x n-4 at center
+# each cell has 8 way => has 8 * (n-4) * (n-4)
+# but duplicated when 2 cell in board n-4 x n-4 => has 8 * (n-4) * (n-4) - f(n-4)
+# check the border, each border has 2 * (n-2) => has 4 * 2 * (n-2) = 8 * (n-2)
+# at 2 border near board n-4 x n-4, each corner has 4 way => has 16 way
+# => has 8 * (n-2) + 16 + 8 * (n-4) * (n-4) - f(n-4)
+# => 8n + 8(n-4)(n-4) - f(n-4) => using dp
