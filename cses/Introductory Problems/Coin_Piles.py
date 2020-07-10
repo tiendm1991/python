88a3f5f39449b1a620b06f2a27196bfb7d0a1ccc
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-07-10 10:33:34 +0700

    Create Coin_Piles.py

---
+++ cses/Introductory Problems/Coin_Piles.py
@@ -0,0 +1,17 @@
+def f(x, y):
+	a = None
+	if 2 * x - y < 0 or (2 * x - y) % 3 != 0:
+		return False
+	a = (2 * x - y) // 3
+	b = x - 2 * a
+	return b >= 0
+# assume choose "a" time 2 coin of pile1, "b" time 2 coin of pile2
+# 2a + b = x (1)
+# 2b + a = y (2)
+# 2 * (1) - (2) => 3a = 2x - y -> a = (2 * x - y) // 3
+n = int(input())
+for i in range(n):
+	x, y = input().split(' ')
+	x, y = int(x), int(y)
+	print("YES" if f(x, y) or f(y, x) else "NO")
+`
