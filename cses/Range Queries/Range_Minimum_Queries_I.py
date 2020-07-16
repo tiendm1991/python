0ab87237b7e08a48b3d2e6d8c626783782eb5762
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-07-16 09:48:59 +0700

    Create Range_Minimum_Queries_I.py

---
+++ cses/Range Queries/Range_Minimum_Queries_I.py
@@ -0,0 +1,37 @@
+class SegmentTree:
+    def __init__(self, arr):
+        self.maxInt = 10 ** 9
+        self.st = [self.maxInt] * 4 * len(arr)
+        for i, v in enumerate(arr):
+            self.update(0, 0, len(arr) - 1, i, v)
+
+    def update(self, id, left, right, i, val):
+        if i < left or i > right:
+            return
+        if left == right:
+            self.st[id] = val
+            return
+        mid = (left + right) // 2
+        self.update(id * 2 + 1, left, mid, i, val)
+        self.update(id * 2 + 2, mid + 1, right, i, val)
+        self.st[id] = min(self.st[id * 2 + 1], self.st[id * 2 + 2])
+
+    def getMin(self, id, left, right, u, v):
+        if u > right or v < left:
+            return self.maxInt
+        if u <= left and v >= right:
+            return self.st[id]
+        mid = (left + right) // 2
+        minLeft = self.getMin(id * 2 + 1, left, mid, u, v)
+        minRight = self.getMin(id * 2 + 2, mid + 1, right, u, v)
+        return min(minLeft, minRight)
+
+
+var = input().split(' ')
+n, q = int(var[0]), int(var[1])
+a = [int(x) for x in input().split()]
+st = SegmentTree(a)
+for i in range(q):
+    var = input().split(' ')
+    l, r = int(var[0]) - 1, int(var[1]) - 1
+    print(st.getMin(0, 0, n - 1, l, r))
