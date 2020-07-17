87c459c2561842a26377ef8545221b2cf62e2332
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-07-17 09:38:49 +0700

    Update Labyrinth2.cpp

--- cses/Graphs/Labyrinth2.cpp
+++ cses/Graphs/Labyrinth2.cpp
@@ -1,21 +1,18 @@
-#include <algorithm>
 #include <iostream>
-#include <vector>
-#include <tuple>
-#include <queue>
- 
+#include <bits/stdc++.h>
+
 #define pairs array<int, 2>
- 
+
 using namespace std;
- 
+
 const int maxN = 1000;
 int n, m;
 string s[maxN];
- 
+
 bool valid(int i, int j) {
     return 0 <= i && i < n && 0 <= j && j < m && s[i][j] != '#';
 }
- 
+
 int main() {
     int ai, aj, bi, bj;
     cin >> n >> m;
@@ -32,12 +29,18 @@ int main() {
             }
         }
     }
- 
+    map<char, pair<int, int>> direct;
+    direct['U'] = make_pair(1, 0);
+    direct['D'] = make_pair(-1, 0);
+    direct['L'] = make_pair(0, 1);
+    direct['R'] = make_pair(0, -1);
+
     queue<pairs > q;
     pairs target = {bi, bj};
     pairs source = {ai, aj};
     q.push(source);
     string ans = "";
+    s[ai][aj] = '#';
     while (!q.empty()) {
         pairs current = q.front();
         q.pop();
@@ -48,43 +51,32 @@ int main() {
         if (valid(i - 1, j)) {
             q.push({i - 1, j});
             trace[i - 1][j] = 'U';
+            s[i - 1][j] = '#';
         }
         if (valid(i + 1, j)) {
             q.push({i + 1, j});
             trace[i + 1][j] = 'D';
+            s[i + 1][j] = '#';
         }
         if (valid(i, j - 1)) {
             q.push({i, j - 1});
             trace[i][j - 1] = 'L';
+            s[i][j - 1] = '#';
         }
         if (valid(i, j + 1)) {
             q.push({i, j + 1});
             trace[i][j + 1] = 'R';
+            s[i][j + 1] = '#';
         }
-        s[i][j] = '#';
     }
     if (trace[bi][bj] == '.') {
         cout << "NO" << "\n";
     } else {
-        int i = bi, j = bj;
-        while (i != ai || j != aj) {
-            ans.push_back(trace[i][j]);
-            if (trace[i][j] == 'U') {
-                i++;
-                continue;
-            }
-			if (trace[i][j] == 'D') {
-                i--;
-                continue;
-            } 
-			if (trace[i][j] == 'L') {
-                j++;
-                continue;
-            } 
-			if (trace[i][j] == 'R') {
-                j--;
-                continue;
-            }
+        while (bi != ai || bj != aj) {
+            ans.push_back(trace[bi][bj]);
+            pair<int, int> next = direct[trace[bi][bj]];
+            bi += next.first;
+            bj += next.second;
         }
         reverse(ans.begin(), ans.end());
         cout << "YES" << "\n";
