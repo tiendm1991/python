72d168afec1bcef8f9cad900083401743e939fb8
TienDM <tiendm1991@gmail.com>
2020-07-19 23:32:12 +0700

    update

---
+++ leetcode/contest/weekly-198/Contest2.py
@@ -0,0 +1,34 @@
+class Solution:
+    def countSubTrees(self, n: int, edges, labels: str):
+        if n == 1:
+            return [1]
+        if n == 0:
+            return []
+        alpha = 'abcdefghijklmnopqrstuvwxyz'
+        d = {i: set() for i in range(n)}
+        memo = {i: {x: 0 for x in alpha} for i in range(n)}
+        for e in edges:
+            d[e[0]].add(e[1])
+            d[e[1]].add(e[0])
+
+        def dfs(i):
+            if visited[i]:
+                return False
+            visited[i] = True
+            memo[i][labels[i]] += 1
+            for child in d[i]:
+                if dfs(child):
+                    for c in alpha:
+                        memo[i][c] += memo[child][c]
+            return True
+
+        visited = [False] * n
+        dfs(0)
+        ans = [0] * n
+        for i in range(n):
+            ans[i] = memo[i][labels[i]]
+        return ans
+
+
+s = Solution()
+print(s.countSubTrees(4, [[0, 2], [0, 3], [1, 2]], "aeed"))
---
+++ leetcode/contest/weekly-198/Contest3.py
@@ -0,0 +1,65 @@
+class Solution:
+    def maxNumOfSubstrings(self, s: str):
+        d = {}
+        alpha = 'abcdefghijklmnopqrstuvwxyz'
+        d_search = {x: [] for x in alpha}
+        visited = {x: False for x in alpha}
+        for i, c in enumerate(s):
+            if c not in d:
+                d[c] = [i, i]
+            else:
+                d[c][1] = i
+            d_search[c].append(i)
+
+        def biSearch(arr, l, h, x, y):
+            if l > h or x > arr[h] or y < arr[l] or x > y:
+                return False
+            m = (l + h) // 2
+            if x <= arr[m] <= y:
+                return True
+            if arr[m] >= y:
+                return biSearch(arr, l, m, x, y)
+            if arr[m] <= x:
+                return biSearch(arr, m + 1, h, x, y)
+            return False
+        i = 0
+        while i < len(s):
+            c = s[i]
+            if visited[c]:
+                i += 1
+                continue
+            r = d[c]
+            repeat = False
+            for c2 in alpha:
+                if c2 not in d or c2 == c:
+                    continue
+                r2 = d[c2]
+                if r[1] < r2[0] or r[0] > r2[1]:
+                    continue
+                arr = d_search[c2]
+                if not biSearch(arr, 0, len(arr) - 1, r[0] + 1, r[1] - 1):
+                    continue
+                r3 = [min(r[0], r2[0]), max(r[1], r2[1])]
+                if r != r3:
+                    d[c] = r3
+                    repeat = True
+                    break
+            if not repeat:
+                visited[c] = True
+                i += 1
+
+        d = {k: v for k, v in sorted(d.items(), key=lambda item: item[1][1])}
+        cur = None
+        ans = []
+        for k in d:
+            if not cur or d[k][0] >= cur[1]:
+                cur = d[k]
+                ans.append(s[cur[0]:cur[1] + 1])
+            if cur[1] == len(s)-1:
+                break
+        return ans
+
+
+s = Solution()
+print(s.maxNumOfSubstrings("dedbeaccbb"))
+print(s.maxNumOfSubstrings("dbcaacadacd"))
