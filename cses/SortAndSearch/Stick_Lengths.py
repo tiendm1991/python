f0d87b383efc862355c6c2f26e2034fb419df065
TienDM <tiendm1991@gmail.com>
2020-07-25 22:20:19 +0700

    update

--- cses/SortAndSearch/Stick_Lengths.py
+++ cses/SortAndSearch/Stick_Lengths.py
@@ -1,7 +1,7 @@
 n = int(input())
 a = [int(c) for c in input().split(' ')]
 a.sort()
-avg = a[n//2] if n % 2 == 1 else (a[n//2] + a[n//2 - 1]) // 2
+avg = a[n//2]
 ans = 0
 for x in a:
     ans += abs(x - avg)
