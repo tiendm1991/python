968818ca0df46bae25521e69002daaa41da65553
TienDM <tiendm1991@gmail.com>
2020-07-25 23:58:48 +0700

    update

---
+++ leetcode/minRotatedSortedArray.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findMin(self, nums) -> int:
+        n = len(nums)
+        if n == 0:
+            return None
+
+        def search(left, right):
+            if right - left == 1:
+                return min(nums[left], nums[right])
+            if left == right:
+                return nums[left]
+            mid = (left + right) // 2
+
+            if nums[left] == nums[mid]:
+                if nums[mid] == nums[right]:
+                    return search(left + 1, right - 1)
+                elif nums[mid] < nums[right]:
+                    return search(left+ 1, mid)
+                else:
+                    return search(mid + 1, right)
+            elif nums[left] < nums[mid]:
+                if nums[mid] <= nums[right]:
+                    return nums[left]
+                else:
+                    return search(mid + 1, right)
+            else:
+                return search(left+1, mid)
+
+        return search(0, n - 1)
+
+
+s = Solution()
+print(s.findMin([3, 3, 1, 3]))
