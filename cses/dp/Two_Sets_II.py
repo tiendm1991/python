0000764a92af8937fa3281a249b0bb6b2010bbcc
TienDM <tiendm1991@gmail.com>
2020-07-27 23:13:47 +0700

    update

---
+++ cses/dp/Two_Sets_II.py
@@ -0,0 +1,12 @@
+n = int(input())
+s = n * (n + 1) // 2
+if s % 2 == 1:
+    print(0)
+else:
+    mod = 10 ** 9 + 7
+    target = s // 2
+    dp = [[1 if j == 0 else 0 for j in range(target + 1)] for i in range(n + 1)]
+    for i in range(1, n + 1):
+        for j in range(1, target + 1):
+            dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - i]) % mod
+    print((dp[n][target] * (mod+1) // 2) % mod)
