8c53a4609064b7e2e32fc49d3f8cf8ab9df6e68b
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-07-28 14:14:38 +0700

    Create Room_Allocation.cpp

---
+++ cses/SortAndSearch/Room_Allocation.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define arr3 array<int, 3>
+int n;
+set<arr3 > s;
+
+int main() {
+    int count = 0;
+    int maxRoom = 0;
+    map<int, int> m;
+    set<int> rooms;
+    cin >> n;
+    int ans[n];
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        s.insert({a, 1, -i});
+        s.insert({b + 1, -1, -i});
+    }
+    for (auto x : s) {
+        count += x[1];
+        maxRoom = max(maxRoom, count);
+        if (x[1] == -1) {
+            rooms.insert(m.find(x[2])->second);
+            m.erase(x[2]);
+        }
+        if (x[1] == 1) {
+            if(rooms.empty()){
+                rooms.insert(count);
+            }
+            int r = *rooms.begin();
+            ans[-x[2]] = r;
+            m.insert({x[2], r});
+            rooms.erase(r);
+        }
+    }
+    cout << maxRoom << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << ans[i];
+        if (i < n - 1) {
+            cout << " ";
+        }
+    }
+
+}
