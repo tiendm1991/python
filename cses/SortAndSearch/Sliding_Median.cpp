5b2d3254a8f1371fa65527db2d95823c681c1113
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-08-08 19:35:27 +0700

    Update Sliding_Median.cpp

--- cses/SortAndSearch/Sliding_Median.cpp
+++ cses/SortAndSearch/Sliding_Median.cpp
@@ -1,44 +1,71 @@
 #include <iostream>
-#include <array>
-#include <set>
-
+#include <bits/stdc++.h>
+ 
 using namespace std;
 #define ll long long
 #define arr2 array<int, 2>
-
+ 
 const ll maxN = 2e5;
-set<arr2 > minHeap, maxHeap;
-int n, k, s, a[maxN];
-
-int add(int x) {
-    int s1 = minHeap.size(), s2 = maxHeap.size();
-    if (k % 2 == 0) {
-
-    } else {
-
+multiset<ll> minHeap, maxHeap;
+ll n, k, a[maxN];
+ 
+void resize() {
+    if (minHeap.size() > maxHeap.size()) {
+        auto it = minHeap.begin();
+        maxHeap.insert(-(*it));
+        minHeap.erase(it);
+    } else if (maxHeap.size() - minHeap.size() > 1) {
+        auto it = maxHeap.begin();
+        minHeap.insert(-(*it));
+        maxHeap.erase(it);
     }
 }
-
-int remove(int x) {
-
+ 
+void add(int x) {
+    if (minHeap.size() == 0 && maxHeap.size() == 0) {
+        minHeap.insert(x);
+    } else if (minHeap.size() > 0 && x >= *minHeap.begin()) {
+        minHeap.insert(x);
+    } else {
+        maxHeap.insert(-x);
+    }
+    resize();
 }
-
+ 
+void remove(int x) {
+    if (x >= *minHeap.begin()) {
+		auto it = minHeap.find(x);
+        minHeap.erase(it);
+    } else {
+		auto it = maxHeap.find(-x);
+        maxHeap.erase(it);
+    }
+    resize();
+}
+ 
 int getMedian() {
-
+    return -(*maxHeap.begin());
 }
-
+ 
 int main() {
     cin >> n >> k;
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
-    for (int i = 1; i < k; i++) {
+    if (k == 1) {
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+        return 0;
+    }
+ 
+    for (int i = 0; i < k - 1; i++) {
         add(a[i]);
     }
-    for (int i = k; i < n; i++) {
+    for (int i = k - 1; i < n; i++) {
         add(a[i]);
         cout << getMedian() << " ";
-        remove(a[i - k]);
+        remove(a[i - k + 1]);
     }
     return 0;
 }
