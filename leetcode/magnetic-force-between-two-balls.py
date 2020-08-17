e1a1e7fc43fc67fa9f9a5f5fc5f7b857ecedd5ec
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-08-17 09:14:02 +0700

    Create magnetic-force-between-two-balls.py

---
+++ leetcode/magnetic-force-between-two-balls.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxDistance(self, a: List[int], m: int) -> int:
+        n = len(a)
+        a.sort()
+
+        def check(d):
+            start = 0
+            m2 = m - 2
+            for i in range(1, n):
+                if m2 == 0:
+                    return a[-1] - a[start] >= d
+                if a[i] - a[start] >= d:
+                    start = i
+                    m2 -= 1
+            return False
+
+        left, right = 1, a[-1] - a[0]
+        while left < right:
+            mid = (left + right + 1) // 2
+            if check(mid):
+                left = mid
+            else:
+                right = mid - 1
+        return left
