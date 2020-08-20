a3a91e776c0fb987f3d55c52cf10cc0f3d3f64ba
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-08-20 22:15:50 +0700

    Create Road_Reparation.cpp

---
+++ cses/Graphs/Road_Reparation.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <set>
+
+using namespace std;
+#define ll long long
+#define arr3 array<int, 3>
+
+const int maxN = 1e5;
+set<arr3 > edges;
+int p[maxN + 1];
+int n, m;
+ll ans;
+
+int find(int u) {
+    while (u != p[u]) {
+        u = p[u];
+    }
+    return u;
+}
+
+void unions(int ru, int rv) {
+    int r = min(ru, rv), c = max(ru, rv);
+    p[c] = r;
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 1; i <= m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        edges.insert({c, a, b});
+    }
+    for (int i = 1; i <= n; i++) {
+        p[i] = i;
+    }
+
+    for (arr3 e: edges) {
+        if (n == 1) {
+            break;
+        }
+        int u = e[1], v = e[2], c = e[0];
+        int ru = find(u), rv = find(v);
+        if (ru != rv) {
+            ans += c;
+            --n;
+            unions(ru, rv);
+        }
+    }
+    if (n > 1) {
+        cout << "IMPOSSIBLE";
+    } else {
+        cout << ans;
+    }
+    return 0;
+}
