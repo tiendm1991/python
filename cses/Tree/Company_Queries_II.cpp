a43142b44e7b41d5b9a855de411ed9bc02f93c84
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-08-20 10:36:00 +0700

    Update Company_Queries_II.cpp

--- cses/Tree/Company_Queries_II.cpp
+++ cses/Tree/Company_Queries_II.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
-
-
+ 
+ 
 using namespace std;
 const int maxN = 2e5;
+bool visited[maxN + 1];
 int n, q, a[maxN + 1], tree[maxN + 1][20], h[maxN + 1];
-
+vector<int> adj[maxN + 1];
+ 
+int dfs(int u) {
+    visited[u] = true;
+    for (int v : adj[u]) {
+        if (!visited[v]) {
+            for (int j = 0; j < 20; j++) {
+                if (j == 0) {
+                    tree[v][j] = a[v];
+                } else {
+                    if (tree[tree[v][j - 1]][j - 1] == 0) {
+                        break;
+                    } else {
+                        tree[v][j] = tree[tree[v][j - 1]][j - 1];
+                    }
+                }
+            }
+            dfs(v);
+        }
+    }
+    return 0;
+}
+ 
+ 
 int lca(int u, int v) {
     if (u == v) {
         return u;
@@ -34,29 +58,19 @@ int lca(int u, int v) {
         }
         i--;
     }
-
+ 
     return a[u];
 }
-
+ 
 int main() {
     cin >> n >> q;
     for (int i = 2; i <= n; i++) {
         cin >> a[i];
         h[i] = h[a[i]] + 1;
+        adj[i].push_back(a[i]);
+        adj[a[i]].push_back(i);
     }
-    for (int i = 1; i <= n; i++) {
-        for (int j = 0; j < 20; j++) {
-            if (j == 0) {
-                tree[i][j] = a[i];
-            } else {
-                if (tree[tree[i][j - 1]][j - 1] == 0) {
-                    break;
-                } else {
-                    tree[i][j] = tree[tree[i][j - 1]][j - 1];
-                }
-            }
-        }
-    }
+    dfs(1);
     for (int i = 0; i < q; i++) {
         int x, k;
         cin >> x >> k;
