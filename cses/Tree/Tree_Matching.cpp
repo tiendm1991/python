5b2d08e7b33324e02e6f766d5e019aa7eae48572
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-08-26 08:58:55 +0700

    Update Tree_Matching.cpp

--- cses/Tree/Tree_Matching.cpp
+++ cses/Tree/Tree_Matching.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <vector>
-
+ 
 using namespace std;
-
+ 
 const int maxN = 2e5;
 int n, dp[2][maxN + 1];
 vector<int> adj[maxN + 1];
-
+ 
 void dfs(int u, int p) {
     vector<int> pre;
     pre.push_back(0);
@@ -15,26 +15,25 @@ void dfs(int u, int p) {
             continue;
         }
         dfs(v, u);
-        // Calculate prefix all answer of child v
-        pre.push_back(pre.back() + max(dp[0][v], dp[1][v]));
+        int maxV = max(dp[0][v], dp[1][v]);
+        pre.push_back(maxV);
+        // Not choose u
+        dp[0][u] += maxV;
     }
-    
-    int len = pre.size();
-    
-    // Not choose u
-    dp[0][u] = pre[len - 1];
 
-    // Choose u
+    int len = pre.size();
+ 
+    // Calculate choose u
     for (int i = 1, j = 1; i < len; i++, j++) {
         if (adj[u][j - 1] == p) {
             ++j;
         }
         int v = adj[u][j - 1];
         // Choose (u, v)
-        dp[1][u] = max(dp[1][u], 1 + dp[0][v] + pre[i - 1] + pre[len - 1] - pre[i]);
+        dp[1][u] = max(dp[1][u], 1 + dp[0][v] + dp[0][u] - pre[i]);
     }
 }
-
+ 
 int main() {
     cin >> n;
     for (int i = 0; i < n - 1; i++) {
