246a87f71ee0fcfb64db479eb8cac4c2c6199c3d
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-09-05 18:27:40 +0700

    Update Range_Update_Queries.cpp

--- cses/Range Queries/Range_Update_Queries.cpp
+++ cses/Range Queries/Range_Update_Queries.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+ 
 using namespace std;
 #define ll long long
 #define ar array
@@ -10,13 +10,13 @@ struct Node {
     ll val; // giá trị lớn nhất.
 } nodes[maxN * 4];
 int n, q;
-
-void down(int id) {
+ 
+void down(int id, int l1, int r1, int l2, int r2) {
     ll t = nodes[id].lazy;
     nodes[2 * id + 1].lazy += t;
-    nodes[2 * id + 1].val += t;
+    nodes[2 * id + 1].val += (r1 - l1 + 1) * t;
     nodes[2 * id + 2].lazy += t;
-    nodes[2 * id + 2].val += t;
+    nodes[2 * id + 2].val += (r2 - l2 + 1) * t;
     nodes[id].lazy = 0;
 }
 
@@ -30,7 +30,7 @@ void update(int id, int left, int right, int u, int v, ll val) {
         return;
     }
     int mid = (left + right) / 2;
-    down(id);
+    down(id, left, mid, mid + 1, right);
     update(id * 2 + 1, left, mid, u, v, val);
     update(id * 2 + 2, mid + 1, right, u, v, val);
     nodes[id].val = nodes[2 * id + 1].val + nodes[2 * id + 2].val;
@@ -44,17 +44,17 @@ ll get(int id, int left, int right, int u, int v) {
         return nodes[id].val;
     }
     int mid = (left + right) / 2;
-    down(id);
+    down(id, left, mid, mid + 1, right);
     return get(id * 2 + 1, left, mid, u, v) + get(id * 2 + 2, mid + 1, right, u, v);
 }
-
+ 
 int main() {
     cin >> n >> q;
     for (int i = 0; i < n; i++) {
         cin >> a[i];
         update(0, 0, n - 1, i, i, a[i]);
     }
-
+ 
     for (int i = 0; i < q; i++) {
         int type;
         cin >> type;
@@ -63,10 +63,10 @@ int main() {
             cin >> a >> b >> u;
             update(0, 0, n - 1, a - 1, b - 1, u);
         } else {
-            ll k;
-            cin >> k;
-            cout << get(0, 0, n - 1, k - 1, k - 1) << "\n";
+            ll u;
+            cin >> u;
+            cout << get(0, 0, n - 1, u - 1, u - 1) << "\n";
         }
     }
-
+ 
 }
