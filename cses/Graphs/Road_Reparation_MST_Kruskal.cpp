2f0bfd93aa088ac13a484c6bef96d1259cfb3ada
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-09-06 13:25:24 +0700

    Update Road_Reparation_MST_Kruskal.cpp

--- cses/Graphs/Road_Reparation_MST_Kruskal.cpp
+++ cses/Graphs/Road_Reparation_MST_Kruskal.cpp
@@ -2,29 +2,29 @@
 #include <vector>
 #include <array>
 #include <set>
-
+ 
 using namespace std;
 #define ll long long
 #define arr3 array<int, 3>
-
+ 
 const int maxN = 1e5;
 set<arr3 > edges;
 int p[maxN + 1];
 int n, m;
 ll ans;
-
+ 
 int find(int u) {
-    while (u != p[u]) {
-        u = p[u];
-    }
-    return u;
+	if (u != p[u]){
+		p[u] = find(p[u]);
+	}
+    return p[u];
 }
-
+ 
 void unions(int ru, int rv) {
     int r = min(ru, rv), c = max(ru, rv);
     p[c] = r;
 }
-
+ 
 int main() {
     cin >> n >> m;
     for (int i = 1; i <= m; i++) {
@@ -35,7 +35,7 @@ int main() {
     for (int i = 1; i <= n; i++) {
         p[i] = i;
     }
-
+ 
     for (arr3 e: edges) {
         if (n == 1) {
             break;
