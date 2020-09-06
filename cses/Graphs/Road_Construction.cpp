87f4fa0bca58e5f7e71c9fdd113f784b72f02ae9
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-09-06 13:24:19 +0700

    Update Road_Construction.cpp

--- cses/Graphs/Road_Construction.cpp
+++ cses/Graphs/Road_Construction.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
 #include <array>
-
+ 
 using namespace std;
-
+ 
 const int maxN = 1e5;
 int p[maxN + 1], s[maxN + 1];
 int n, m, maxSize = 1;
-
+ 
 int find(int u) {
-    while (u != p[u]) {
-        u = p[u];
-    }
-    return u;
+	if (u != p[u]){
+		p[u] = find(p[u]);
+	}
+    return p[u];
 }
-
+ 
 void unions(int ru, int rv) {
     int r = min(ru, rv), c = max(ru, rv);
     p[c] = r;
     s[r] += s[c];
     maxSize = max(maxSize, s[r]);
 }
-
+ 
 int main() {
     cin >> n >> m;
-
+ 
     for (int i = 1; i <= n; i++) {
         p[i] = i;
         s[i] = 1;
     }
-
+ 
     for (int i = 0; i < m; i++) {
         int a, b;
         cin >> a >> b;
