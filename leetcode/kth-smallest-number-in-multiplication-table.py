e7caecaaa9610c434bfd1a5bedba4a4fff653809
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-09-16 09:14:26 +0700

    Create kth-smallest-number-in-multiplication-table.py

---
+++ leetcode/kth-smallest-number-in-multiplication-table.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findKthNumber(self, m: int, n: int, k: int) -> int:
+        
+        def count(x):
+            ans = 0
+            for i in range(1, m+1):
+                ans += min(x // i, n)
+            return ans
+        
+        left,right = 1, m * n
+        while left < right:
+            mid = (left + right) // 2
+            if count(mid) < k:
+                left = mid + 1
+            else:
+                right = mid
+        return left
