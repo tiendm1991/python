c8f12a1b20058440db7db24ae2e06a16ba4ead9b
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-09-21 12:17:39 +0700

    Create redundant-connection.py

---
+++ leetcode/redundant-connection.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        p = [i for i in range(n+1)]
+        def find(v):
+            if v != p[v]:
+                p[v] = find(p[v])
+            return p[v]
+        
+        for x, y in edges:
+            rx = find(x)
+            ry = find(y)
+            if rx == ry:
+                return [x, y]
+            p[ry] = rx
+        return None
