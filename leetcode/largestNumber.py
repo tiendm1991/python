d00e8473405355c7a94d3f04b4119ecfa6156450
tiendm <tiendm@tcbs.com.vn>
2020-09-25 16:28:10 +0700

    update

---
+++ leetcode/largestNumber.py
@@ -0,0 +1,14 @@
+import functools
+class Solution:
+    def largestNumber(self, nums) -> str:
+        def compare(x, y):
+            return int(str(y) + str(x)) - int(str(x) + str(y))
+
+        nums = sorted(nums, key=functools.cmp_to_key(compare))
+        ans = ""
+        for x in nums:
+            ans += str(x)
+        return str(int(ans))
+
+s = Solution()
+print(s.largestNumber([3, 30, 34, 5, 9]))
