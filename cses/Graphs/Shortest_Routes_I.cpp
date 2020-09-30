2ed0030cfcf28efe5a50409a3f5bafbca8286fba
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-09-30 11:02:39 +0700

    Update Shortest_Routes_I.cpp

--- cses/Graphs/Shortest_Routes_I.cpp
+++ cses/Graphs/Shortest_Routes_I.cpp
@@ -6,7 +6,6 @@ using namespace std;
 const int maxN = 1e5;
 const ll maxV = 1e15;
 ll a[maxN + 1], ans[maxN + 1];
-bool visited[maxN+1];
 vector<array<ll, 2>> adj[maxN + 1];
 int n, m;
 
@@ -19,22 +18,22 @@ int main() {
     for (int i = 1; i <= m; i++) {
 		int a, b, c;
 		cin >> a >> b >> c;
-        adj[a].push_back({b, c});
+        adj[a].push_back({c, b});
     }
 	//min heap
 	priority_queue <array<ll, 2> , vector<array<ll, 2>>,greater<array<ll, 2>>> pq;
 	pq.push({0, 1});
+	ans[1] = 0;
 	while(!pq.empty()){
 		array<ll, 2> cur = pq.top();
 		pq.pop();
-		if(visited[cur[1]]){
+		if(cur[0] > ans[cur[1]]){
 			continue;
 		}
-		ans[cur[1]] = cur[0];
-		visited[cur[1]] = true;
 		for(array<ll, 2> x: adj[cur[1]]){
-			if(!visited[x[0]]){
-				pq.push({cur[0] + x[1], x[0]});
+			if(cur[0] + x[0] < ans[x[1]]){
+				ans[x[1]] = cur[0] + x[0];
+				pq.push({ans[x[1]], x[1]});
 			}
 		}
 	}
