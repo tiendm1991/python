2f5de29d88d3033bdc68e7dcdf77e4f35bbb5ff3
tiendm.hust <tiendm1991@users.noreply.github.com>
2020-10-03 09:29:46 +0700

    Create reach-a-number.py

---
+++ leetcode/reach-a-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reachNumber(self, target: int) -> int:
+        target = abs(target)
+        delta = 1 + 8 * target
+        n = math.ceil((-1 + math.sqrt(delta)) / 2)
+        A = n * (n + 1) // 2
+        if A == target:
+            return n
+        while (A - target) % 2 == 1:
+            n += 1
+            A += n
+        return n
+        
