e0dc295c08781776a9febeec5c4c971949c76adc
tiendm <tiendm@tcbs.com.vn>
2020-10-07 16:11:36 +0700

    update

--- leetcode/couples-holding-hands.py
+++ leetcode/couples-holding-hands.py
@@ -4,19 +4,21 @@ class Solution:
         p = {row[i]: i for i in range(n)}
         ans = 0
         for i in range(0, n, 2):
-            neighbor = i + 1
+            couple = i + 1
             pos = p[i]
-            posNeighbor = p[neighbor]
+            posCouple = p[couple]
             if pos % 2 == 0:
-                if posNeighbor != pos + 1:
+                posNeighbor, neighbor = pos + 1, row[pos + 1]
+                if posCouple != pos + 1:
                     ans += 1
-                    p[row[pos + 1]], p[neighbor] = p[neighbor], p[row[pos + 1]]
-                    row[pos + 1], row[posNeighbor] = row[posNeighbor], row[pos + 1]
+                    p[neighbor], p[couple] = posCouple, posNeighbor
+                    row[posNeighbor], row[posCouple] = couple, neighbor
             else:
-                if posNeighbor != pos - 1:
+                posNeighbor, neighbor = pos - 1, row[pos - 1]
+                if posCouple != pos - 1:
                     ans += 1
-                    p[row[pos - 1]], p[neighbor] = p[neighbor], p[row[pos - 1]]
-                    row[pos - 1], row[posNeighbor] = row[posNeighbor], row[pos - 1]
+                    p[neighbor], p[couple] = posCouple, posNeighbor
+                    row[posNeighbor], row[posCouple] = couple, neighbor
         return ans
 
 
