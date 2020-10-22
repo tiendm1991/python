6ad899d68db6ce87a465431add7a26624d794cc9
tiendm <tiendm@tcbs.com.vn>
2020-10-22 18:08:22 +0700

    update

--- race-car.py
+++ race-car.py
@@ -13,11 +13,10 @@ class Solution:
                 return n
             dp[x] = n + 1 + help((2 ** n - 1) - x)
             for i in range(1, n):
-                dp[x] = min(dp[x], i + 2 + help(x - (2 ** i - 1)))
                 start = 2 ** i - 1
                 j = 0
-                while start - 2 ** j > 0:
-                    dp[x] = min(dp[x], i + 1 + (j + 1) + 1 + help(x - (start - 2 ** j)))
+                while start - (2 ** j - 1) > 0:
+                    dp[x] = min(dp[x], i + 2 + j + help(x - (start - (2 ** j - 1))))
                     j += 1
             return dp[x]
 
@@ -25,6 +24,7 @@ class Solution:
 
 
 s = Solution()
+print(s.racecar(10))
 print(s.racecar(20))
 # print(s.racecar(5))
 # print(s.racecar(4))
