f6600f6abe137c291f7b9f9c2531090474916010
tiendm <tiendm@tcbs.com.vn>
2020-11-03 11:38:17 +0700

    update

--- leetcode/bicontest/bcontest-038/bContest3.py
+++ leetcode/bicontest/bcontest-038/bContest3.py
@@ -5,26 +5,18 @@ class Solution:
         ans = 0
         for i in range(m):
             for j in range(n):
-                if s[i] == t[j]:
-                    k = 1
-                    while i + k < m and j + k < n and s[i + k] == t[j + k]:
-                        k += 1
-                    if i + k < m and j + k < n:
-                        ans += 1
-                        k += 1
-                        while i + k < m and j + k < n and s[i + k] == t[j + k]:
-                            ans += 1
-                            k += 1
-                else:
+                k = 0
+                while i + k < m and j + k < n and s[i + k] == t[j + k]:
+                    k += 1
+                if i + k < m and j + k < n:
                     ans += 1
-                    k = 1
+                    k += 1
                     while i + k < m and j + k < n and s[i + k] == t[j + k]:
                         ans += 1
                         k += 1
-
         return ans
 
 
 s = Solution()
 print(s.countSubstrings("bab", "bbb"))
-# print(s.countSubstrings("aa", "bb"))
+print(s.countSubstrings("aa", "bb"))
--- leetcode/bicontest/bcontest-038/bContest4.py
+++ leetcode/bicontest/bcontest-038/bContest4.py
@@ -1,27 +1,32 @@
-# https://leetcode.com/problems/fancy-sequence/
-
-class Fancy:
-
-    def __init__(self):
-        return
-
-    def append(self, val: int) -> None:
-        return
-
-    def addAll(self, inc: int) -> None:
-        return
-
-    def multAll(self, m: int) -> None:
-        return
-
-    def getIndex(self, idx: int) -> int:
-        return 1
+import string
 
 
-# Your Fancy object will be instantiated and called as such:
-obj = Fancy()
-obj.append(2)
-obj.addAll(3)
-obj.append(7)
-obj.multAll(3)
-print(obj.getIndex(4))
+class Solution:
+    def numWays(self, words, target: str) -> int:
+        mod = 10 ** 9 + 7
+        n = len(words[0])
+        m = len(target)
+        countCharAt = {c: [0] * n for c in string.ascii_lowercase}
+        for w in words:
+            for i, c in enumerate(w):
+                countCharAt[c][i] += 1
+        dp = [[-1 for j in range(m)] for i in range(n)]
+
+        def help(idxW, idxT):
+            if idxT == m:
+                return 1
+            if idxW == n:
+                return 0
+            if dp[idxW][idxT] != -1:
+                return dp[idxW][idxT]
+            ans = help(idxW + 1, idxT)
+            if countCharAt[target[idxT]][idxW] != 0:
+                ans = (ans + countCharAt[target[idxT]][idxW] * help(idxW + 1, idxT + 1)) % mod
+            dp[idxW][idxT] = ans
+            return ans
+
+        return help(0, 0)
+
+
+s = Solution()
+print(s.numWays(["acca", "bbbb", "caca"], "aba"))
