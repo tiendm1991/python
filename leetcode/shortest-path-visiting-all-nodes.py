25ad639f4e896f23169208d1d2ae398d735d8d79
tiendm <tiendm@tcbs.com.vn>
2020-11-05 17:39:56 +0700

    update

---
+++ leetcode/shortest-path-visiting-all-nodes.py
@@ -0,0 +1,28 @@
+class Solution:
+    def shortestPathLength(self, graph) -> int:
+        n = len(graph)
+        target = (1 << n) - 1
+        if n <= 2:
+            return n - 1
+        q = []
+        visited = set()
+        for i in range(n):
+            q.append([1 << i, i])
+            visited.add((1 << i, i))
+        count = 0
+        while q:
+            newQ = []
+            for cur in q:
+                if cur[0] == target:
+                    return count
+                for nex in graph[cur[1]]:
+                    if (cur[0], nex) not in visited:
+                        newQ.append([cur[0] | 1 << nex, nex])
+                        visited.add((cur[0] | 1 << nex, nex))
+            count += 1
+            q = newQ
+        return 0
+
+
+s = Solution()
+print(s.shortestPathLength([[1, 2, 3], [0], [0], [0]]))
