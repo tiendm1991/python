e273b909ec7b66af9dff920a543e24c6233da2f2
TienDM <tiendm1991@gmail.com>
2020-11-13 22:22:11 +0700

    update

--- leetcode/mirror-reflection.py
+++ leetcode/mirror-reflection.py
@@ -1,8 +1,16 @@
+import math
+
+
 class Solution:
     def mirrorReflection(self, p: int, q: int) -> int:
-        k = 1
+        g = math.gcd(p, q)
+        m, n = q // g, p // g
+        if n % 2 == 0:
+            return 2
+        else:
+            return m % 2
         return -1
 
 
 s = Solution()
-print(s.mirrorReflection())
+print(s.mirrorReflection(3, 2))
