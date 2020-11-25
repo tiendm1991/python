a5ff87d535b41613994b4126c162bb568df5c4af
TienDM <tiendm1991@gmail.com>
2020-11-25 22:38:52 +0700

    update

--- leetcode/reachable-nodes-in-subdivided-graph.py
+++ leetcode/reachable-nodes-in-subdivided-graph.py
@@ -12,24 +12,25 @@ class Solution:
             d[i][j] = k
             d[j][i] = k
         q = [(-m, 0)]
-        dis = {}
+        seen = {}
+        # seen[i] means that we can arrive at node i and have seen[i] moves left
         while q:
             x, i = heapq.heappop(q)
-            if i in dis:
+            if i in seen:
                 continue
-            dis[i] = -x
+            seen[i] = -x
             if i not in d:
                 continue
             for j in d[i]:
                 x2 = -x - d[i][j] - 1
-                if j not in dis and x2 >= 0:
+                if j not in seen and x2 >= 0:
                     heapq.heappush(q, (-x2, j))
-        res = len(dis)
+        res = len(seen)
         for i, j, k in edges:
-            res += min(dis.get(i, 0) + dis.get(j, 0), k)
-        return dis, res
+            res += min(seen.get(i, 0) + seen.get(j, 0), k)
+        return seen, res
 
 
 s = Solution()
 print(s.reachableNodes([[0, 1, 10], [0, 2, 1], [1, 2, 2]], 6, 3))
-# print(s.reachableNodes([[0, 1, 4], [1, 2, 6], [0, 2, 8], [1, 3, 1]], 10, 4))
+print(s.reachableNodes([[0, 1, 4], [1, 2, 6], [0, 2, 8], [1, 3, 1]], 10, 4))
