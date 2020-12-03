ca95b4a7ff10f79cb279b088cb2fba247a56faf9
TienDM <tiendm1991@gmail.com>
2020-12-03 23:17:07 +0700

    update

--- leetcode/valid-permutations-for-di-sequence.py
+++ leetcode/valid-permutations-for-di-sequence.py
@@ -6,14 +6,19 @@ class Solution:
             return 1
         dp = [[1 if i == 0 else 0 for j in range(n + 1)] for i in range(n + 1)]
         for i in range(n):
-            if s[i] == 'I'
-                for j in range(i, n - ):
-                    dp[i + 1][j] = (dp[i + 1][j] + dp[i][j]) % mod
+            if s[i] == 'I':
+                cur = 0
+                for j in range(n - i):
+                    cur = (cur + dp[i][j]) % mod
+                    dp[i + 1][j] = cur
             else:
-                for j in range(i):
-                    dp[i + 1][j] = (dp[i + 1][j] + dp[i][j]) % mod
-        return dp
+                cur = 0
+                for j in range(n - i - 1, -1, -1):
+                    cur = (cur + dp[i][j + 1]) % mod
+                    dp[i + 1][j] = cur
+        return dp[n][0]
 
 
 s = Solution()
 print(s.numPermsDISequence("DID"))
+print(s.numPermsDISequence("DIDD"))
