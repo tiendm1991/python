05a09ea2f5984da2d8175c6d6d0faefffbd3bc70
TienDM <tiendm1991@gmail.com>
2020-12-06 16:50:06 +0700

    update

--- leetcode/smallest-range-ii.py
+++ leetcode/smallest-range-ii.py
@@ -5,20 +5,21 @@ class Solution:
             return 0
         a.sort()
         res = a[-1] - a[0]
-        mi, ma = a[1], a[-1]
+        mn, mx = a[1], a[-1]
+        minmin = a[0] + 2 * k
         for i in range(n - 1):
+            mn = min(a[i + 1], minmin)
             x = a[i] + 2 * k
-            if x > ma:
-                res = min(res, x - mi)
-                ma = x
-                mi = a[i + 1]
-            elif x < mi:
-                res = min(res, ma - x)
-                mi = min(a[i + 1], mi)
-            else:
-                mi = a[i + 1]
+            if x <= mn:
+                res = min(res, mx - x)
+                break
+            if x >= mx:
+                mx = x
+            res = min(res, mx - mn)
         return res
 
 
 s = Solution()
 print(s.smallestRangeII([1, 3, 6], 3))
+print(s.smallestRangeII([1, 3, 10], 4))
+print(s.smallestRangeII([1, 8, 15, 17], 3))
