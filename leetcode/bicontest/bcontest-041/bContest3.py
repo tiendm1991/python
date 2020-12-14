b2f0d7ecf749dac84f9d29c684f61f6e52ae3035
tiendm <tiendm@tcbs.com.vn>
2020-12-14 14:51:26 +0700

    update

--- leetcode/bicontest/bcontest-041/bContest3.py
+++ leetcode/bicontest/bcontest-041/bContest3.py
@@ -1,6 +1,18 @@
 class Solution:
     def stoneGameVI(self, a, b) -> int:
-
+        n = len(a)
+        sa, sb = sum(a), sum(b)
+        s = sorted([(a[i] + b[i], a[i], b[i]) for i in range(n)], reverse=True)
+        for i, x in enumerate(s):
+            if i % 2 == 0:
+                sb -= x[2]
+            else:
+                sa -= x[1]
+        if sa > sb:
+            return 1
+        elif sa < sb:
+            return -1
+        return 0
 
 s = Solution()
 print(s.stoneGameVI([1, 3], [2, 1]))
