0f96de00a56a4ec2d30acc30e31eba93ac709002
tiendm <tiendm@tcbs.com.vn>
2020-12-16 17:50:18 +0700

    update

---
+++ leetcode/validate-stack-sequences.py
@@ -0,0 +1,19 @@
+class Solution:
+    def validateStackSequences(self, pushed, popped) -> bool:
+        n = len(pushed)
+        stack = []
+        i, j = 0, 0
+        while i < n:
+            while not stack or stack[-1] != popped[i]:
+                if j == n:
+                    return False
+                stack.append(pushed[j])
+                j += 1
+            stack.pop()
+            i += 1
+        return True
+
+
+s = Solution()
+# print(s.validateStackSequences([1, 2, 3, 4, 5], [4, 5, 3, 2, 1]))
+print(s.validateStackSequences([1, 2, 3, 4, 5], [4, 3, 5, 1, 2]))
