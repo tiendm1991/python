319b11efda170bcc80ed9ebc9b8032a8dc302731
tiendm <tiendm@tcbs.com.vn>
2020-12-16 17:50:38 +0700

    update

--- leetcode/validate-stack-sequences.py
+++ leetcode/validate-stack-sequences.py
@@ -15,5 +15,5 @@ class Solution:
 
 
 s = Solution()
-# print(s.validateStackSequences([1, 2, 3, 4, 5], [4, 5, 3, 2, 1]))
+print(s.validateStackSequences([1, 2, 3, 4, 5], [4, 5, 3, 2, 1]))
 print(s.validateStackSequences([1, 2, 3, 4, 5], [4, 3, 5, 1, 2]))
