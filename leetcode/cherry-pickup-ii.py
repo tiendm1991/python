349979c2360917567b97e1c197121ed432ac49fc
TienDM <tiendm1991@gmail.com>
2020-12-23 21:56:12 +0700

    update

--- leetcode/cherry-pickup-ii.py
+++ leetcode/cherry-pickup-ii.py
@@ -1,5 +1,5 @@
 class Solution:
-    def cherryPickup(self, a) -> int:
+    def cherryPickup_topdown(self, a) -> int:
         m, n = len(a), len(a[0])
         dp = {}
 
@@ -19,8 +19,28 @@ class Solution:
 
         return helper(0, 0, n - 1)
 
+    def cherryPickup(self, a) -> int:
+        m, n = len(a), len(a[0])
+        dp = [[[0 for k in range(n + 2)] for j in range(n + 2)] for i in range(m + 1)]
+        for r in range(m - 1, -1, -1):
+            for c1 in range(1, n + 1):
+                for c2 in range(1, n + 1):
+                    dp[r][c1][c2] = a[r][c1 - 1]
+                    if c1 != c2:
+                        dp[r][c1][c2] += a[r][c2 - 1]
+                    preMax = 0
+                    for i in range(-1, 2):
+                        for j in range(-1, 2):
+                            preMax = max(preMax, dp[r + 1][c1 + i][c2 + j])
+                    dp[r][c1][c2] += preMax
+        return dp[0][1][n]
+
 
 s = Solution()
+print(s.cherryPickup([[4, 1, 5, 7, 1],
+                      [6, 0, 4, 6, 4],
+                      [0, 9, 6, 3, 5]]))
 print(s.cherryPickup([[3, 1, 1], [2, 5, 1], [1, 5, 5], [2, 1, 1]]))
+print(s.cherryPickup([[1, 0, 0, 3], [0, 0, 0, 3], [0, 0, 3, 3], [9, 0, 3, 3]]))
 print(s.cherryPickup([[1, 0, 0, 0, 0, 0, 1], [2, 0, 0, 0, 0, 3, 0], [2, 0, 9, 0, 0, 0, 0], [0, 3, 0, 5, 4, 0, 0],
                       [1, 0, 2, 3, 0, 0, 6]]))
