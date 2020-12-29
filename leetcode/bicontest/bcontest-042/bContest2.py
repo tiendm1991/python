31158171f5ff147c8fa8846a5434d4713c63c83c
tiendm <tiendm@tcbs.com.vn>
2020-12-29 09:18:11 +0700

    update

--- leetcode/bicontest/bcontest-042/bContest2.py
+++ leetcode/bicontest/bcontest-042/bContest2.py
@@ -1,16 +1,14 @@
 class Solution:
-    def getSumAbsoluteDifferences(self, nums):
-        n = len(nums)
-        pre = [0] * (n + 1)
-        for i in range(1, n + 1):
-            pre[i] = pre[i - 1] + nums[i - 1]
-        res = [0] * n
-        for i in range(n):
-            # res[i] = (i + 1) * nums[i] - pre[i + 1] + pre[-1] - pre[i + 1] - (n - i - 1) * nums[i]
-            res[i] = i * nums[i] - pre[i] + pre[-1] - pre[i] - (n - i) * nums[i]
-        return res
+    def averageWaitingTime(self, a) -> float:
+        res = 0
+        cur = a[0][0]
+        for start, prepare in a:
+            cur = max(cur, start)
+            cur += prepare
+            res += cur - start
+        return res / len(a)
 
 
 s = Solution()
-print(s.getSumAbsoluteDifferences([2, 3, 5]))
-print(s.getSumAbsoluteDifferences([1, 4, 6, 8, 10]))
+print(s.averageWaitingTime([[5, 2], [5, 4], [10, 3], [20, 1]]))
+print(s.averageWaitingTime([[1, 2], [2, 5], [4, 3]]))
