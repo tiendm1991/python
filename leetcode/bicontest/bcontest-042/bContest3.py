7906fbc4bcd623dc31a2543fab797e8a525bdb4a
tiendm <tiendm@tcbs.com.vn>
2020-12-29 11:03:58 +0700

    update

--- leetcode/bicontest/bcontest-042/bContest3.py
+++ leetcode/bicontest/bcontest-042/bContest3.py
@@ -1,36 +1,30 @@
 class Solution:
-    def stoneGameVI(self, a, b) -> int:
-        n = len(a)
-        sa, sb = sum(a), sum(b)
-        s = sorted([(a[i] + b[i], a[i], b[i]) for i in range(n)], reverse=True)
-        for i, x in enumerate(s):
-            if i % 2 == 0:
-                sb -= x[2]
+    def maximumBinaryString(self, binary: str) -> str:
+        n = len(binary)
+        if n == 1:
+            return binary
+        i = binary.find('0')
+        if i == -1:
+            return binary
+        j = i + 1
+        a = [c for c in binary]
+        while j < n:
+            if a[j] == '1':
+                j += 1
             else:
-                sa -= x[1]
-        if sa > sb:
-            return 1
-        elif sa < sb:
-            return -1
-        return 0
-
-    def stoneGameVI2(self, a, b) -> int:
-        n = len(a)
-        sa, sb = 0, 0
-        s = sorted([(a[i] + b[i], a[i], b[i]) for i in range(n)], reverse=True)
-        for i, x in enumerate(s):
-            if i % 2 == 0:
-                sa += x[1]
-            else:
-                sb += x[2]
-        if sa > sb:
-            return 1
-        elif sa < sb:
-            return -1
-        return 0
+                if j == i + 1:
+                    a[i] = '1'
+                    i = j
+                    j += 1
+                else:
+                    a[i] = '1'
+                    a[i + 1] = '0'
+                    a[j] = '1'
+                    i += 1
+                    j += 1
+        return ''.join(a)
 
 
 s = Solution()
-print(s.stoneGameVI([1, 3], [2, 1]))
-print(s.stoneGameVI([1, 2], [3, 1]))
-print(s.stoneGameVI([2, 4, 3], [1, 6, 7]))
+print(s.maximumBinaryString("000110"))
+# print(s.maximumBinaryString("01110"))
