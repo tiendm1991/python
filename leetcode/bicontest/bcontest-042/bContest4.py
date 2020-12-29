35a6c82a5f8982f314440b470b787a322dadc3fb
tiendm <tiendm@tcbs.com.vn>
2020-12-29 16:26:56 +0700

    update

--- leetcode/bicontest/bcontest-042/bContest4.py
+++ leetcode/bicontest/bcontest-042/bContest4.py
@@ -1,22 +1,26 @@
 class Solution:
     def minMoves(self, nums, k: int) -> int:
-        n = len(nums)
-        count = 0
-        i, j = nums.index(1), 0
-        res = n
-        while i < n and j < n:
-            count += nums[j]
-            if count == k:
-                res = min(res, j - i + 1 - k)
-                i += 1
-                while i < n and nums[i] == 0:
-                    i += 1
-                count -= 1
-            j += 1
+        a = []
+        for i, x in enumerate(nums):
+            if x == 1:
+                a.append(i)
+        n = len(a)
+        res = float("inf")
+        for i in range(n - k + 1):
+            j = i + k - 1
+            m = (i + j) // 2
+            sub_res = 0
+            for delta in range(1, m - i + 1):
+                sub_res += (a[m] - delta) - a[m - delta]
+            for delta in range(1, j - m + 1):
+                sub_res += a[m + delta] - (a[m] + delta)
+            res = min(res, sub_res)
         return res
 
 
 s = Solution()
-print(s.minMoves([1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1], 7))
+print(s.minMoves([0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1], 6))
+# print(s.minMoves([1, 0, 0, 1, 0, 1], 2))
+# print(s.minMoves([1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1], 7))
 # print(s.minMoves([1, 0, 0, 0, 0, 0, 1, 1], 3))
 # print(s.minMoves([1, 0, 0, 0, 1, 0, 1, 0, 0, 1], 3))
