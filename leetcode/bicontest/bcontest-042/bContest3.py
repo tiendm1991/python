a23710d259ad5980af15932bb8bc8104b425fa54
tiendm <tiendm@tcbs.com.vn>
2020-12-29 11:09:06 +0700

    update

--- leetcode/bicontest/bcontest-042/bContest3.py
+++ leetcode/bicontest/bcontest-042/bContest3.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maximumBinaryString(self, binary: str) -> str:
+    def maximumBinaryString_long(self, binary: str) -> str:
         n = len(binary)
         if n == 1:
             return binary
@@ -24,7 +24,28 @@ class Solution:
                     j += 1
         return ''.join(a)
 
+    def maximumBinaryString(self, binary: str) -> str:
+        n = len(binary)
+        if n == 1:
+            return binary
+        i = binary.find('0')
+        if i == -1:
+            return binary
+        j = i + 1
+        a = [c for c in binary]
+        while j < n:
+            if a[j] == '1':
+                j += 1
+            else:
+                a[i] = '1'
+                a[i + 1] = '0'
+                if j > i + 1:
+                    a[j] = '1'
+                i += 1
+                j += 1
+        return ''.join(a)
+
 
 s = Solution()
-print(s.maximumBinaryString("000110"))
-# print(s.maximumBinaryString("01110"))
+print(s.maximumBinaryString_long("000110"), s.maximumBinaryString("000110"))
+print(s.maximumBinaryString_long("01110"), s.maximumBinaryString("01110"))
