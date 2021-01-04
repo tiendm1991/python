7c26a019e4cf65fe003e7a1a8a65aa097a71c7a9
tiendm <tiendm@tcbs.com.vn>
2021-01-04 17:29:29 +0700

    update

--- leetcode/contest/weekly-222/Contest3.py
+++ leetcode/contest/weekly-222/Contest3.py
@@ -1,45 +1,38 @@
 class Solution:
-    def findBall(self, grid):
-        m, n = len(grid), len(grid[0])
-        a = [[1 for j in range(3 * n)] for i in range(3 * m)]
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    for k in range(3):
-                        a[3 * i + k][3 * j + k] = 0
-                elif grid[i][j] == -1:
-                    for k in range(3):
-                        a[3 * i + k][3 * j + 2 - k] = 0
+    def waysToSplit(self, nums) -> int:
+        mod = 10 ** 9 + 7
+        n = len(nums)
+        res = 0
+        pre = [0] * (n + 1)
+        for i in range(1, n + 1):
+            pre[i] = pre[i - 1] + nums[i - 1]
 
-        def bfs(y0):
-            q = set()
-            for i in range(3):
-                if a[0][3 * y0 + i] == 1:
-                    q.add((0, 3 * y0 + i))
-            while q:
-                newQ = set()
-                for p in q:
-                    if p[0] == 3 * m - 1:
-                        return p[1] // 3
-                    if (p[0] + 1, p[1]) not in newQ and a[p[0] + 1][p[1]] == 1:
-                        newQ.add((p[0] + 1, p[1]))
-                        if (p[0] + 1, p[1] - 1) not in newQ and 0 <= p[1] - 1 and a[p[0] + 1][p[1] - 1] == 1:
-                            newQ.add((p[0] + 1, p[1] - 1))
-                        if (p[0] + 1, p[1] + 1) not in newQ and p[1] + 1 < 3 * n and a[p[0] + 1][p[1] + 1] == 1:
-                            newQ.add((p[0] + 1, p[1] + 1))
-                q = newQ
-            return -1
+        def biSearch(l, r):
+            s3 = pre[n] - pre[r]
+            l1, r1 = l, r
+            while l1 < r1:
+                m = (l1 + r1 + 1) // 2
+                s1, s2 = pre[m], pre[r] - pre[m]
+                if s1 <= s2 <= s3:
+                    l1 = m
+                else:
+                    r1 = m - 1
+            l2, r2 = l, r
+            while l2 < r2:
+                m = (l2 + r2) // 2
+                s1, s2 = pre[m], pre[r] - pre[m]
+                if s1 <= s2 <= s3:
+                    r2 = m
+                else:
+                    l2 = m + 1
+            return max(l1 - l2 + 1, 0)
 
-        res = [-1] * n
-        for i in range(n):
-            res[i] = bfs(i)
+        for i2 in range(n - 1, 1, -1):
+            res = (res + biSearch(1, i2)) % mod
+        return res
+
         return res
 
 
 s = Solution()
-print(s.findBall([[-1]]))
-print(s.findBall([[1, 1, 1, -1, -1],
-                  [1, 1, 1, -1, -1],
-                  [-1, -1, -1, 1, 1],
-                  [1, 1, 1, 1, -1],
-                  [-1, -1, -1, -1, -1]]))
+print(s.waysToSplit([1, 2, 2, 2, 5, 0]))
