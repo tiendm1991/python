954415300e17188c496bd6b7de896beceb2c6f67
TienDM <tiendm1991@gmail.com>
2021-01-09 23:26:37 +0700

    update

--- leetcode/bicontest/bcontest-043/bContest3.py
+++ leetcode/bicontest/bcontest-043/bContest3.py
@@ -1,7 +1,6 @@
 class Solution:
     def constructDistancedSequence(self, n: int):
-        d = {i: 2 for i in range(2, n + 1)}
-        d[1] = 1
+        choose = set(range(1, n + 1))
 
         def backtrack(a, i):
             if i == 2 * n - 1:
@@ -9,19 +8,18 @@ class Solution:
             if a[i] != 0:
                 return backtrack(a, i + 1)
             for x in range(n, 0, -1):
+                if x not in choose:
+                    continue
                 if x == 1:
-                    if d[x] == 1:
-                        d[x] -= 1
-                        a[i] = 1
-                        res = backtrack(a, i + 1)
-                        if res:
-                            return res
-                        a[i] = 0
-                        d[x] += 1
-                    else:
-                        return None
-                if d[x] == 2 and i + x < 2 * n - 1 and a[i + x] == 0:
-                    d[x] -= 2
+                    choose.remove(x)
+                    a[i] = 1
+                    res = backtrack(a, i + 1)
+                    if res:
+                        return res
+                    a[i] = 0
+                    choose.add(x)
+                if i + x < 2 * n - 1 and a[i + x] == 0:
+                    choose.remove(x)
                     a[i] = x
                     a[i + x] = x
                     res = backtrack(a, i + 1)
@@ -29,8 +27,8 @@ class Solution:
                         return res
                     a[i] = 0
                     a[i + x] = 0
-                    d[x] += 2
-            return
+                    choose.add(x)
+            return None
 
         return backtrack([0] * (2 * n - 1), 0)
 
