576db4cbaac88f79faf600224702fdef962be3cc
tiendm <tiendm@tcbs.com.vn>
2021-01-14 15:04:44 +0700

    update

---
+++ leetcode/broken-calculator.py
@@ -0,0 +1,19 @@
+class Solution:
+    def brokenCalc(self, x: int, y: int) -> int:
+        res = 0
+        while y > x:
+            res += 1
+            if y % 2 == 0:
+                y //= 2
+            else:
+                y += 1
+        res += x - y
+        return res
+
+
+s = Solution()
+print(s.brokenCalc(3, 10))
+print(s.brokenCalc(68, 71))
+print(s.brokenCalc(1, 10))
+print(s.brokenCalc(5, 8))
+print(s.brokenCalc(1000000000, 1))
