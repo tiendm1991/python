7f9ba7b3778848216bed64715c454cc38868066c
tiendm <tiendm@tcbs.com.vn>
2021-01-14 18:43:34 +0700

    update

--- leetcode/contest/weekly-215/Contest3.py
+++ leetcode/contest/weekly-215/Contest3.py
@@ -1,5 +1,5 @@
 class Solution:
-    def minOperations(self, nums, x: int) -> int:
+    def minOperations_bisearch(self, nums, x: int) -> int:
         n = len(nums)
         a, b = [0], [0]
         for i in nums:
@@ -29,7 +29,24 @@ class Solution:
                         r = m - 1
         return ans if ans <= n else -1
 
+    def minOperations(self, nums, x: int) -> int:
+        n = len(nums)
+        if n == 1:
+            return 1 if nums[0] == x else -1
+        pre, suf = [0] * (n + 1), [0] * (n + 1)
+        res = n + 1
+        d = {}
+        for i in range(1, n + 1):
+            pre[i] = pre[i - 1] + nums[i - 1]
+            suf[i] = suf[i - 1] + nums[n - i]
+            d[suf[i]] = i
+        for i in range(1, n + 1):
+            sufFind = x - pre[i]
+            if sufFind in d and i < n - d[sufFind] + 1:
+                res = min(res, i + d[sufFind])
+        return res if res <= n else -1
+
 
 s = Solution()
 print(s.minOperations([1, 1, 4, 2, 3], 5))
-print(s.minOperations([3, 2, 20, 1, 1, 3], 10))
+# print(s.minOperations([3, 2, 20, 1, 1, 3], 10))
