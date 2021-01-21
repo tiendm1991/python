7dda723c6af92ab1324216f526d9bc289a7c2032
TienDM <tiendm1991@gmail.com>
2021-01-21 20:54:02 +0700

    update

--- leetcode/2021/convert-to-base-2.py
+++ leetcode/2021/convert-to-base-2.py
@@ -1,4 +1,15 @@
 class Solution:
+    def baseNeg2_2(self, n: int) -> str:
+        if n == 0:
+            return "0"
+        res = ""
+        b = -2
+        while n != 0:
+            m = n % b
+            res = str(-m) + res
+            n = (n + m) // b
+        return res
+
     def baseNeg2(self, n: int) -> str:
         if n == 0:
             return "0"
