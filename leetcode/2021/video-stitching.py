f93f3d596787452b95d2f87cb8ef9491a6c06807
TienDM <tiendm1991@gmail.com>
2021-01-23 14:47:55 +0700

    update

--- leetcode/2021/video-stitching.py
+++ leetcode/2021/video-stitching.py
@@ -3,33 +3,49 @@ import functools
 
 class Solution:
     def videoStitching(self, clips, t: int) -> int:
+        if t == 0:
+            return 0
+
         def compare(c1, c2):
             if c1[0] != c2[0]:
                 return c1[0] - c2[0]
             return c1[1] - c2[1]
 
-        a = sorted(clips, key=functools.cmp_to_key(compare))
-        if a[0][0] > 0 or a[-1][1] < t:
+        b = sorted(clips, key=functools.cmp_to_key(compare))
+        if b[0][0] > 0 or b[-1][1] < t:
             return -1
-        res = [a[0]]
-        for i in range(1, len(a)):
-            if res[-1][0] <= a[i][0] and a[i][1] <= res[-1][1]:
-                continue
-            elif a[i][0] == res[-1][0]:
-                res[-1][1] = a[i][1]
-            elif a[i][0] > res[-1][1]:
-                return -1
+        a = [[b[0][0], b[0][1]]]
+        for i in range(1, len(b)):
+            if b[i][0] == a[-1][0]:
+                a[-1][1] = b[i][1]
             else:
-                res.append(a[i])
-                if a[i][1] >= t:
-                    break
-        return len(res)
+                a.append(b[i][::])
+        end = a[0][1]
+        res = 1
+        i = 1
+        while i < len(a):
+            if end >= t:
+                break
+            j = i
+            endTmp = end
+            while j < len(a) and a[j][0] <= end:
+                endTmp = max(endTmp, a[j][1])
+                j += 1
+            if j == i:
+                return -1
+            res += 1
+            end = endTmp
+            i = j
+        return res
 
 
 s = Solution()
-# print(s.videoStitching([[0, 2], [1, 6], [3, 10]], 10))
-# print(s.videoStitching([[0, 2], [4, 8]], 5))
-# print(s.videoStitching([[0, 2], [4, 6], [8, 10], [1, 9], [1, 5], [5, 9]], 10))
+print(s.videoStitching([[16, 18], [16, 20], [3, 13], [1, 18], [0, 8], [5, 6], [13, 17], [3, 17], [5, 6]], 15))
+print(s.videoStitching([[5, 7], [1, 8], [0, 0], [2, 3], [4, 5], [0, 6], [5, 10], [7, 10]], 5))
+print(s.videoStitching([[0, 2], [3, 10], [8, 10], [1, 9], [1, 5], [5, 9]], 10))
+print(s.videoStitching([[0, 2], [1, 6], [3, 10]], 10))
+print(s.videoStitching([[0, 2], [4, 8]], 5))
+print(s.videoStitching([[0, 2], [4, 6], [8, 10], [1, 9], [1, 5], [5, 9]], 10))
 print(s.videoStitching(
     [[0, 1], [6, 8], [0, 2], [5, 6], [0, 4], [0, 3], [6, 7], [1, 3], [4, 7], [1, 4], [2, 5], [2, 6], [3, 4], [4, 5],
      [5, 7], [6, 9]], 9))
