b6af22a3b5ae8b6496155680c53c2c6299b91661
tiendm <tiendm@tcbs.com.vn>
2021-01-25 11:59:51 +0700

    update

--- leetcode/2021/contest/weekly-225/Contest4.py
+++ leetcode/2021/contest/weekly-225/Contest4.py
@@ -1,61 +1,28 @@
-import functools
-
-
 class Solution:
-    def canMouseWin(self, grid, catJump: int, mouseJump: int) -> bool:
-        m, n = len(grid), len(grid[0])
-        direct = [[0, -1], [-1, 0], [0, 1], [1, 0]]
-        iCat, iMouse, target = None, None, None
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "M":
-                    iMouse = (i, j)
-                elif grid[i][j] == "C":
-                    iCat = (i, j)
-                elif grid[i][j] == "F":
-                    target = (i, j)
+    def minimumBoxes(self, n: int) -> int:
+        def count(h):
+            return h * (h + 1) * (h + 2) // 6
 
-        @functools.lru_cache(None)
-        def solve(turn, pCat, pMouse):
-            rCat, cCat = pCat
-            rMouse, cMouse = pMouse
-            if pMouse == target:
-                return True
-            if pCat == target or pCat == pMouse or turn > 100:
-                return False
-            if turn % 2 == 0:
-                for d in direct:
-                    for j in range(1, mouseJump + 1):
-                        rNext, cNext = rMouse + d[0] * j, cMouse + d[1] * j
-                        if 0 <= rNext < m and 0 <= cNext < n and grid[rNext][cNext] != '#':
-                            if solve(turn + 1, pCat, (rNext, cNext)):
-                                return True
-                        else:
-                            break
-                if solve(turn + 1, pCat, pMouse):
-                    return True
-                return False
+        l, r = 1, n
+        while l < r:
+            m = (l + r) // 2
+            if count(m) >= n:
+                r = m
             else:
-                for d in direct:
-                    for j in range(1, catJump + 1):
-                        rNext, cNext = rCat + d[0] * j, cCat + d[1] * j
-                        if 0 <= rNext < m and 0 <= cNext < n and grid[rNext][cNext] != '#':
-                            if not solve(turn + 1, (rNext, cNext), pMouse):
-                                return False
-                        else:
-                            break
-                if not solve(turn + 1, pCat, pMouse):
-                    return False
-                return True
-
-        return solve(0, iCat, iMouse)
+                l = m + 1
+        res = l * (l + 1) // 2
+        s = count(l)
+        while s - l >= n:
+            res -= 1
+            s -= l
+            l -= 1
+        return res
 
 
 s = Solution()
-print(s.canMouseWin(["####.##",
-                     ".#C#F#.",
-                     "######.",
-                     "##M.###"], 3, 6))
-print(s.canMouseWin(["####F",
-                     "#C...",
-                     "M...."], 1, 2))
+print(s.minimumBoxes(60))
+print(s.minimumBoxes(64))
+print(s.minimumBoxes(16))
+print(s.minimumBoxes(3))
+print(s.minimumBoxes(4))
+print(s.minimumBoxes(10))
