aa939b113b443947042902fb6ee87d123485ec92
tiendm <tiendm@tcbs.com.vn>
2021-01-26 15:35:57 +0700

    update

--- leetcode/2021/escape-a-large-maze.py
+++ leetcode/2021/escape-a-large-maze.py
@@ -5,21 +5,29 @@ class Solution:
     def isEscapePossible(self, blocked, source, target) -> bool:
         n = 10 ** 6
         direct = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-        blocked = {tuple(b) for b in blocked}
-        q = collections.deque()
-        q.append(source)
-        visited = {tuple(b) for b in blocked}
-        visited.add(tuple(source))
-        while q:
-            x, y = q.popleft()
-            if [x, y] == target:
-                return True
-            for d in direct:
-                if 0 <= x + d[0] < n and 0 <= y + d[1] < n and (x + d[0], y + d[1]) not in visited:
-                    q.append([x + d[0], y + d[1]])
-                    visited.add((x + d[0], y + d[1]))
-        return False
+
+        def bfs(s, t):
+            q = collections.deque()
+            q.append(s)
+            visited = {tuple(b) for b in blocked}
+            visited.add(tuple(s))
+            count = 1
+            while q:
+                x, y = q.popleft()
+                if [x, y] == t:
+                    return True
+                for d in direct:
+                    if 0 <= x + d[0] < n and 0 <= y + d[1] < n and (x + d[0], y + d[1]) not in visited:
+                        q.append([x + d[0], y + d[1]])
+                        visited.add((x + d[0], y + d[1]))
+                if len(q) + count >= 20000:
+                    return True
+                count += 1
+            return False
+
+        return bfs(source, target) and bfs(target, source)
 
 
 s = Solution()
-print(s.isEscapePossible([[0, 1], [1, 0]], [0, 0], [0, 2]))
+print(s.isEscapePossible([[0, 1],
+                          [1, 0]], [0, 0], [0, 2]))
